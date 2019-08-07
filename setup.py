2b97290e9893ae8bcd76cf446a6f78509375c849
Robert Wilson <rwi@pmpc1454.npm.ac.uk>
2019-08-07 08:52:13 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='nchack',
+      version='0.1',
+      description='A python wrapper for CDO and NCDO',
+      url='tbc',
+      author='Robert Wilson',
+      author_email='rwi@pml.ac.uk',
+      license='MIT',
+      packages=['nchack'],
+      zip_safe=False)
+
+
+
