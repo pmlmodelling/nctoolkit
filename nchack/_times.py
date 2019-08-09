33b8bd8b47ef1a2eec45018150dc3c0a573422c0
Robert Wilson <rwi@pmpc1454.npm.ac.uk>
2019-08-09 11:08:17 +0100

    add times method

--- nchack/_times.py
+++ nchack/_times.py
@@ -6,7 +6,8 @@ import os
 import tempfile
 import itertools
 
-def times(ff):
+def times(self):
+    ff = self.current
     cdo_result = os.popen( "cdo showtimestamp " + ff).read()
     cdo_result = cdo_result.replace("\n", "")
     cdo_result = cdo_result.split()
