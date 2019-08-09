7139f0eed465117db8ab895722b1739965e6e972
Robert Wilson <rwi@pmpc1454.npm.ac.uk>
2019-08-09 10:27:43 +0100

    minor code tidy up

--- nchack/_ensrange.py
+++ nchack/_ensrange.py
@@ -1,4 +1,8 @@
 
+# to do
+# think about adding ability to call nco
+
+
 import xarray as xr
 import pandas as pd
 import numpy as np
@@ -15,10 +19,13 @@ from ._cleanup import cleanup
 def ensemble_range(self, vars = None):
     """Function to calculate an ensemble range from a list of files"""
     ff_ensemble = self.current
+
     if type(ff_ensemble) is not list:
         raise ValueError("The current state of the tracker is not a list")
+
     self.target = tempfile.NamedTemporaryFile().name + ".nc"
     owd = os.getcwd()
+
    # log the full path of the file
     global nc_created
     nc_created.append(self.target)
@@ -28,18 +35,18 @@ def ensemble_range(self, vars = None):
     else:
         if type(vars) is str:
             vars = [vars]
+
         vars_list = str_flatten(vars, ",")
+    #    vars_list = vars_list.replace("_", "\_")
+
         cdo_command = ("cdo selname," + vars_list + " -ensrange " + str_flatten(ff_ensemble, " ") + " " + self.target) 
 
-
     self.history.append(cdo_command)
-    os.system(cdo_command) 
+    cdo_result = os.system(cdo_command) 
     self.current = self.target 
 
     # clean up the directory
     cleanup(keep = self.current)
 
     return(self)
-    
-
     
