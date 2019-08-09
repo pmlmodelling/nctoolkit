08f32189626b646a00f0f55ba9113f91d5d6d077
Robert Wilson <rwi@pmpc1454.npm.ac.uk>
2019-08-09 09:30:05 +0100

    add one sentence description to to_xarray

--- nchack/_toxarray.py
+++ nchack/_toxarray.py
@@ -5,12 +5,13 @@ import os
 from ._cleanup import cleanup
 
 def to_xarray(self, decode_times = True, remove = False):
+    """convert tracker to xarray data set"""
     ff = self.current
     data = xr.open_dataset(ff, decode_times = decode_times)
 
     cleanup(keep = self.current)
 
-    if self.current != self.start and remove:
+    if (self.current != self.start) and remove:
         os.remove(self.current)
 
     return(data)
