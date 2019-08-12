3073f1fa0a6e329fcf1a44943c91024bd6b6476e
Robert Wilson <rwi@pmpc1454.npm.ac.uk>
2019-08-12 17:08:35 +0100

    make cleanup only way to delete temp files

--- nchack/_cdo_command.py
+++ nchack/_cdo_command.py
@@ -11,19 +11,17 @@ from ._clip import clip
 from ._filetracker import nc_created
 from ._cleanup import cleanup 
 
-def cdo_command(self, command, remove = True):
+def cdo_command(self, command):
     """ Function to all any cdo command of the the form 'command + infile + outfile'"""
 
     self.target = tempfile.NamedTemporaryFile().name + ".nc"
     nc_created.append(self.target)
 
-    cdo_command = "cdo " + command + " " + self.current + " " + self.target
+    cdo_command = command + " " + self.current + " " + self.target
 
     os.system(cdo_command)
     self.history.append(cdo_command)
 
-    if self.current != self.start and remove:
-        os.remove(self.current)
     self.current = self.target
 
     cleanup(keep = self.current)
