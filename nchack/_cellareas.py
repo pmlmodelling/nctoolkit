14e918c0721098cd46d50465c881751138b51e8c
Robert Wilson <rwi@pmpc1454.npm.ac.uk>
2019-08-12 17:08:50 +0100

    make cleanup only way to delete temp files

--- nchack/_cellareas.py
+++ nchack/_cellareas.py
@@ -9,16 +9,15 @@ import itertools
 from ._cleanup import cleanup
 from ._filetracker import nc_created
 
-def cellareas(self, remove = True):
+def cellareas(self):
     self.target = tempfile.NamedTemporaryFile().name + ".nc"
     nc_created.append(self.target)
     cdo_call = ( "cdo gridarea " + self.current + " " + self.target)
     self.history.append(cdo_call)
     os.system(cdo_call)
-    if self.current != self.start and remove:
-        os.remove(self.current)
+
     self.current = self.target
-    cleanup(keep = self.curren)
+    cleanup(keep = self.current)
     
     return(self)
 
