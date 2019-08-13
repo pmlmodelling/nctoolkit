4e0a5de3f2aff6194df118f34fa32e9831e83ba6
Robert Wilson <rwi@pmpc1454.npm.ac.uk>
2019-08-13 15:25:30 +0100

    add ability to send list of files

--- nchack/_cdo_command.py
+++ nchack/_cdo_command.py
@@ -10,15 +10,21 @@ from ._depths import nc_depths
 from ._clip import clip
 from ._filetracker import nc_created
 from ._cleanup import cleanup 
+from .flatten import str_flatten 
 
 def cdo_command(self, command):
     """ Function to all any cdo command of the the form 'command + infile + outfile'"""
 
     self.target = tempfile.NamedTemporaryFile().name + ".nc"
     nc_created.append(self.target)
+    if type(self.current) == list:
+        infile = str_flatten(self.current, " ")
+    else:
+        infile = self.current
 
-    cdo_command = command + " " + self.current + " " + self.target
+    cdo_command = "cdo " + command + " " + infile + " " + self.target
 
+    self.history.append(cdo_command)
     os.system(cdo_command)
     self.history.append(cdo_command)
 
