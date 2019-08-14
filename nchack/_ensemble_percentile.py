970b0593756236f3c3567d2c40545b1a49b01bd5
Robert Wilson <robertwilson190@gmail.com>
2019-08-14 12:44:55 +0100

    add ensemble_percentile method

---
+++ nchack/_ensemble_percentile.py
@@ -0,0 +1,52 @@
+
+# to do
+# think about adding ability to call nco
+
+
+import xarray as xr
+import pandas as pd
+import numpy as np
+import os
+import tempfile
+import itertools
+
+from .flatten import str_flatten
+from ._depths import nc_depths 
+from ._variables import variables
+from ._filetracker import nc_created
+from ._cleanup import cleanup
+
+def ensemble_percentile(self, p, vars = None):
+    """Function to calculate an ensemble range from a list of files"""
+    ff_ensemble = self.current
+
+    if type(ff_ensemble) is not list:
+        raise ValueError("The current state of the tracker is not a list")
+
+    self.target = tempfile.NamedTemporaryFile().name + ".nc"
+    owd = os.getcwd()
+
+   # log the full path of the file
+    global nc_created
+    nc_created.append(self.target)
+
+    if vars is None:
+        cdo_command = ("cdo enspctl," + str(p) + " " + str_flatten(ff_ensemble, " ") + " " + self.target) 
+    else:
+        if type(vars) is str:
+            vars = [vars]
+
+        vars_list = str_flatten(vars, ",")
+    #    vars_list = vars_list.replace("_", "\_")
+
+        cdo_command = ("cdo -enspctl," + str(p) + " " + "selname," + vars_list + " " + str_flatten(ff_ensemble, " ") + " " + self.target) 
+
+    self.history.append(cdo_command)
+    cdo_result = os.system(cdo_command) 
+    self.current = self.target 
+
+    # clean up the directory
+    cleanup(keep = self.current)
+
+    return(self)
+    
