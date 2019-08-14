32d92c90434b1c569e49f500f6ec96293fa4e6b2
Robert Wilson <robertwilson190@gmail.com>
2019-08-14 18:34:44 +0100

    add season climatologies methods

---
+++ nchack/_seasclim.py
@@ -0,0 +1,52 @@
+
+import xarray as xr
+import pandas as pd
+import numpy as np
+import os
+import tempfile
+import itertools
+
+from .flatten import str_flatten
+from ._depths import nc_depths 
+from ._variables import variables
+from ._filetracker import nc_created
+from ._cleanup import cleanup
+
+def seasstat(self, vars = None, stat = "mean"):
+    """Function to calculate the seasonal statistic from a function""" 
+    ff = self.current
+
+    self.target = tempfile.NamedTemporaryFile().name + ".nc"
+
+    global nc_created
+    nc_created.append(self.target)
+
+    if vars is None:
+        cdo_command = ("cdo -yseas" + stat + " " + ff + " " + self.target) 
+    else:
+        if type(vars) is str:
+            vars = [vars]
+        vars_list = str_flatten(vars)
+        cdo_command = ("cdo -yseas" + stat + " -selname," +  vars_list + " " + ff + " " + self.target) 
+
+    self.history.append(cdo_command)
+    os.system(cdo_command) 
+    self.current = self.target 
+
+    # clean up the directory
+    cleanup(keep = self.current)
+
+    return(self)
+    
+
+def seasonal_mean_climatology(self, vars = None):
+    return seasstat(self, vars = vars, stat = "mean")
+
+def seasonal_min_climatology(self, vars = None):
+    return seasstat(self, vars = vars, stat = "min")
+
+def seasonal_max_climatology(self, vars = None):
+    return seasstat(self, vars = vars, stat = "max")
+    
+def seasonal_range_climatology(self, vars = None):
+    return seasstat(self, vars = vars, stat = "range")
