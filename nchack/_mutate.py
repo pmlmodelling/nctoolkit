6e168ad725ee2caaf3ca9dc58102e4e9d5c2709c
Robert Wilson <robertwilson190@gmail.com>
2019-08-15 10:01:02 +0100

    switch to using run_command in system calls

--- nchack/_mutate.py
+++ nchack/_mutate.py
@@ -8,6 +8,7 @@ import itertools
 
 from ._cleanup import cleanup
 from ._filetracker import nc_created
+from ._runcommand import run_command
 
 def mutate(self, operations = None):
     """Function to mutate a netcdf file using expr"""
@@ -34,7 +35,7 @@ def mutate(self, operations = None):
 
     cdo_call = ("cdo aexpr," + expr + " " + self.current  + " " + self.target)
     self.history.append(cdo_call)
-    os.system(cdo_call)
+    run_command(cdo_call)
 
     if os.path.isfile(self.target) == False:
         raise ValueError("Application of expr did not work. Check output")
