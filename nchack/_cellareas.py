7679d3bc3fdd77b6d2cd855f9ed44b0d9ac426c5
Robert Wilson <robertwilson190@gmail.com>
2019-08-15 09:57:29 +0100

    switch to using run_command in system calls

--- nchack/_cellareas.py
+++ nchack/_cellareas.py
@@ -8,13 +8,14 @@ import itertools
 
 from ._cleanup import cleanup
 from ._filetracker import nc_created
+from ._runcommand import run_command
 
 def cellareas(self):
     self.target = tempfile.NamedTemporaryFile().name + ".nc"
     nc_created.append(self.target)
     cdo_call = ( "cdo gridarea " + self.current + " " + self.target)
     self.history.append(cdo_call)
-    os.system(cdo_call)
+    run_command(cdo_call)
 
     self.current = self.target
     cleanup(keep = self.current)
