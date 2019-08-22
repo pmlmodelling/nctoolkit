b33450bc058080bdc044d9078e9e65cde7995201
Robert Wilson <robertwilson190@gmail.com>
2019-08-22 17:05:49 +0100

    add show_months method

--- nchack/_show.py
+++ nchack/_show.py
@@ -11,9 +11,21 @@ from ._filetracker import nc_created
 from ._runcommand import run_command
 
 def show_years(self):
-
     cdo_result = os.popen( "cdo showyear " + self.current).read()
     cdo_result = cdo_result.replace("\n", "")
     cdo_result = cdo_result.split()
-    cdo_result = pd.Series( (v for v in cdo_result) )
+    cdo_result = list(set(cdo_result))
+    cdo_result =  [int(v) for v in cdo_result]
+    cdo_result.sort()
+    return(cdo_result)
+
+
+def show_months(self):
+    cdo_result = os.popen( "cdo showmon " + self.current).read()
+    cdo_result = cdo_result.replace("\n", "")
+    cdo_result = cdo_result.split()
+    cdo_result = list(set(cdo_result))
+    cdo_result.sort()
+    cdo_result = [int(v) for v in cdo_result]
+    cdo_result.sort()
     return(cdo_result)
