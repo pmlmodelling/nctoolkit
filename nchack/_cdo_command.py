38eb49174a7d294af4a5bb7d87067195050c629a
Robert Wilson <robertwilson190@gmail.com>
2019-08-23 16:22:59 +0100

    simplify target file handling

--- nchack/_cdo_command.py
+++ nchack/_cdo_command.py
@@ -16,19 +16,19 @@ from ._runcommand import run_command
 def cdo_command(self, command, silent = True):
     """ Function to all any cdo command of the the form 'command + infile + outfile'"""
 
-    self.target = tempfile.NamedTemporaryFile().name + ".nc"
-    nc_created.append(self.target)
+    target = tempfile.NamedTemporaryFile().name + ".nc"
+    nc_created.append(target)
     if type(self.current) == list:
         infile = str_flatten(self.current, " ")
     else:
         infile = self.current
 
-    cdo_command = "cdo " + command + " " + infile + " " + self.target
+    cdo_command = "cdo " + command + " " + infile + " " + target
 
     self.history.append(cdo_command)
     run_command(cdo_command, self, silent)
 
-    if self.run: self.current = self.target
+    if self.run: self.current = target
 
     cleanup(keep = self.current)
 
