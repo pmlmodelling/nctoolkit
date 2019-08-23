c9ccc0651678dac6bfc39a01707782490bd7b7d1
Robert Wilson <robertwilson190@gmail.com>
2019-08-23 15:43:54 +0100

    add silent option

--- nchack/_seasstat.py
+++ nchack/_seasstat.py
@@ -35,13 +35,13 @@ def seasstat(self, stat = "mean", silent = True):
     
 
 def seasonal_mean(self, silent = True):
-    return seasstat(self, stat = "mean", silent)
+    return seasstat(self, stat = "mean", silent = True)
 
 def seasonal_min(self, silent = True):
-    return seasstat(self, stat = "min", silent)
+    return seasstat(self, stat = "min", silent = True)
 
 def seasonal_max(self, silent = True):
-    return seasstat(self, stat = "max", silent)
+    return seasstat(self, stat = "max", silent = True)
     
 def seasonal_range(self, silent = True):
-    return seasstat(self, stat = "range", silent)
+    return seasstat(self, stat = "range", silent = True)
