05abdd809904a15179d585ce56adf4775b609427
Robert Wilson <robertwilson190@gmail.com>
2019-08-23 11:22:33 +0100

    add ens_nco.py file

---
+++ nchack/_ens_nco.py
@@ -0,0 +1,45 @@
+
+import xarray as xr
+import pandas as pd
+import numpy as np
+import os
+import tempfile
+import itertools
+
+from .flatten import str_flatten
+from ._depths import nc_depths 
+from ._variables import variables
+from ._filetracker import nc_created
+from ._cleanup import cleanup
+from ._runcommand import run_command
+
+def ensemble_nco(self, method):
+    """Function to calculate an ensemble min from a list of files"""
+    ff_ensemble = self.current
+    if type(ff_ensemble) is not list:
+        raise ValueError("The current state of the tracker is not a list")
+    self.target = tempfile.NamedTemporaryFile().name + ".nc"
+    owd = os.getcwd()
+   # log the full path of the file
+    global nc_created
+    nc_created.append(self.target)
+
+    nco_command = ("ncea -y " + method + " " + str_flatten(ff_ensemble, " ") + " " + self.target) 
+
+    self.history.append(nco_command)
+    run_command(nco_command, self) 
+    if self.run: self.current = self.target 
+
+    # clean up the directory
+    cleanup(keep = self.current)
+
+    return(self)
+    
+
+def ensemble_min(self):
+    return ensemble_nco(self, "min")
+
+
+
+
+    
