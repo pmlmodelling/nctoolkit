9babdd03c4a2532decb3974620ed82139b267558
Robert Wilson <robertwilson190@gmail.com>
2019-08-23 14:56:48 +0100

    add method descriptions

--- nchack/_ens_nco.py
+++ nchack/_ens_nco.py
@@ -13,11 +13,11 @@ from ._filetracker import nc_created
 from ._cleanup import cleanup
 from ._runcommand import run_command
 
-def ensemble_nco(self, method):
+def ensemble_nco(self, method, silent = True):
+    """Method to calculate an ensemble stat from a list of files"""
     if self.release == False:
         raise ValueError("NCO methods cannot be held over")
 
-    """Function to calculate an ensemble min from a list of files"""
     ff_ensemble = self.current
     if type(ff_ensemble) is not list:
         raise ValueError("The current state of the tracker is not a list")
@@ -30,7 +30,7 @@ def ensemble_nco(self, method):
     nco_command = ("ncea -y " + method + " " + str_flatten(ff_ensemble, " ") + " " + self.target) 
 
     self.history.append(nco_command)
-    run_command(nco_command, self) 
+    run_command(nco_command, self, silent) 
     if self.run: self.current = self.target 
 
     # clean up the directory
@@ -39,14 +39,18 @@ def ensemble_nco(self, method):
     return(self)
     
 
-def ensemble_min(self):
+def ensemble_min(self, silent = True):
+    """Method to calculate an ensemble min from a list of files"""
     return ensemble_nco(self, "min")
 
-def ensemble_max(self):
+def ensemble_max(self, silent = True):
+    """Method to calculate an ensemble max from a list of files"""
     return ensemble_nco(self, "mean")
 
-def ensemble_mean(self):
+def ensemble_mean(self, silent = True):
+    """Method to calculate an ensemble mean from a list of files"""
     return ensemble_nco(self, "mean")
 
 
-    
+
+
