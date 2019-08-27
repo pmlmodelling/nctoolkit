a6a51d9a2144a38badeb7127f8b0e860e75bba0a
Robert Wilson <robertwilson190@gmail.com>
2019-08-27 13:48:26 +0100

    remove unnecessary imports

---
+++ nchack/_nco_command.py
@@ -0,0 +1,26 @@
+
+import os
+import tempfile
+
+from ._depths import nc_depths
+from ._clip import clip
+from ._filetracker import nc_created
+from ._cleanup import cleanup 
+from ._runcommand import run_command
+
+def nco_command(self, command):
+    """ Function to all any nco command of the the form 'command + infile + outfile'"""
+    target = tempfile.NamedTemporaryFile().name + ".nc"
+    nc_created.append(target)
+    nco_command = command + " " + self.current + " " + target
+    run_command(nco_command)
+    self.history.append(ndo_command)
+
+    if self.run: self.current = target
+
+    cleanup(keep = self.current)
+
+    return(self)
+
+
+
