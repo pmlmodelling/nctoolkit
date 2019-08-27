7663598b199ab861118d98909570c9e4a52973e3
Robert Wilson <robertwilson190@gmail.com>
2019-08-27 13:53:24 +0100

    remove unnecessary imports

--- nchack/_surface.py
+++ nchack/_surface.py
@@ -1,13 +1,8 @@
 
-import xarray as xr
-import pandas as pd
-import numpy as np
 import os
 import tempfile
 import itertools
 
-from ._depths import nc_depths
-from ._clip import clip
 from ._filetracker import nc_created
 from ._cleanup import cleanup 
 from ._runcommand import run_command
