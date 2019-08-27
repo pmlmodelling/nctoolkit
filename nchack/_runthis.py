7fd2917302cc96b3ccd021326aa2c6ab0efc830b
Robert Wilson <robertwilson190@gmail.com>
2019-08-27 16:14:11 +0100

    add run_this command. To replace run_command

---
+++ nchack/_runthis.py
@@ -0,0 +1,73 @@
+import os
+import copy
+import tempfile
+
+from ._filetracker import nc_created
+
+
+def run_this(os_command, self, silent = False):
+    """ Function to run an nco/cdo system command and check output was generated"""
+    # Works on multiple files
+    run = self.run
+    # Step one
+
+    # Step 2: run the system command
+
+    if run:
+
+        if type(self.current) is str:
+            # single file case
+            if silent:
+                os_command = os_command.replace("cdo ", "cdo -s ")
+
+            target = tempfile.NamedTemporaryFile().name + ".nc"
+            nc_created.append(target)
+            os_command = os_command + " " + self.current + " " + target
+            self.history.append(os_command)
+
+            os.system(os_command)
+            
+            # check the file was actually created
+            # Raise error if it wasn't
+
+            if os.path.exists(target) == False:
+                raise ValueError(os_command + " was not successful. Check output")
+            self.current = target
+        else:
+            # multiple file case
+            target_list = []
+            for ff in self.current:
+            
+                if silent:
+                    ff_command = os_command.replace("cdo ", "cdo -s ")
+
+                target = tempfile.NamedTemporaryFile().name + ".nc"
+                nc_created.append(target)
+                ff_command = ff_command + " " + ff + " " + target
+
+                self.history.append(ff_command)
+                os.system(ff_command)
+                
+                # check the file was actually created
+                # Raise error if it wasn't
+
+                if os.path.exists(target) == False:
+                    raise ValueError(ff_command + " was not successful. Check output")
+                target_list.append(target) 
+
+            self.current = copy.deepcopy(target_list)
+
+    else:
+        # Now, if this is not a cdo command we need throw an error
+
+        if os_command.strip().startswith("cdo") == False:
+            raise ValueError("You can only use cdo commands in hold mode")
+        # Now, we need to throw an error if the command is generating a grid
+        
+        commas = [x for x in os_command.split(" ") if "," in x]
+        commas = "".join(commas)
+        if "gen" in commas:
+            raise ValueError("You cannot generate weights as part of a chain!")
+
+
+
