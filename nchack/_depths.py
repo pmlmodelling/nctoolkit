94a1a0bc0b69cfb0bf33b18b1415a6f710eceda1
Robert Wilson <robertwilson190@gmail.com>
2019-08-27 13:43:33 +0100

    remove unnecessary imports

--- nchack/_depths.py
+++ nchack/_depths.py
@@ -1,13 +1,8 @@
 
-import xarray as xr
-import pandas as pd
-import numpy as np
 import os
-import tempfile
-import itertools
-# function to get the depths available in a netcdf file
 
 def nc_depths(ff):
+    """function to get the depths available in a netcdf file"""
     cdo_result = os.popen( "cdo showlevel " + ff).read()
     cdo_result = cdo_result.replace("\n", "").split()
     cdo_result = pd.Series( (float(v) for v in cdo_result) )
