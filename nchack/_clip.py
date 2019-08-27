f231c4b9d8c7eb2f54d88594bc53813d4f6eb611
Robert Wilson <robertwilson190@gmail.com>
2019-08-27 16:17:48 +0100

    switch to using run_this method

--- nchack/_clip.py
+++ nchack/_clip.py
@@ -1,22 +1,13 @@
 
 import tempfile
 
-from ._runcommand import run_command
+from ._runthis import run_this
 from .flatten import str_flatten
 from ._filetracker import nc_created
 from ._cleanup import cleanup
 
 def clip(self, lon_range = [-180, 180], lat_range = [-90, 90], silent = True):
     """ Function to clip netcdf files, spatially"""
-    if type(self.current) is not str:
-        raise ValueError("The current state of the tracker is not a single file")
-
-    target = tempfile.NamedTemporaryFile().name + ".nc"
-    
-    global nc_created
-
-    nc_created.append(target)
-    
     if (type(lon_range) is not list) or (type(lat_range) is not list):
         raise ValueError("Check that lon/lat ranges are tuples")
     
@@ -37,11 +28,9 @@ def clip(self, lon_range = [-180, 180], lat_range = [-90, 90], silent = True):
     if lon_range[0] > -180 or lon_range[1] > 180 or lat_range[0] > -90 or lat_range[1] < 90:
 
         lat_box = str_flatten(lon_range + lat_range)
-        cdo_command = ("cdo sellonlatbox," + lat_box + " " + self.current + " " + target)
-        self.history.append(cdo_command)
-        run_command(cdo_command, self, silent)
+        cdo_command = ("cdo sellonlatbox," + lat_box)
+        run_this(cdo_command, self, silent)
 
-        if self.run: self.current = target 
 
     # clean up the directory
     cleanup(keep = self.current)
