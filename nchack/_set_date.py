0e0d3e655dcd0837c56b27363a93a1ce85834e5c
Robert Wilson <robertwilson190@gmail.com>
2019-09-02 16:08:53 +0100

    switch to using run_this

--- nchack/_set_date.py
+++ nchack/_set_date.py
@@ -5,19 +5,11 @@ import tempfile
 from .flatten import str_flatten
 from ._filetracker import nc_created
 from ._cleanup import cleanup
-from ._runcommand import run_command
+from ._runthis import run_this
 
 def set_date(self, year, month, day, base_year = 1900, silent = True):
-    """Function to set the missing values"""
-    """This is either a range or a single value"""
+    """Function to set the date"""
 
-    ff = self.current
-
-    target = tempfile.NamedTemporaryFile().name + ".nc"
-    owd = os.getcwd()
-   # log the full path of the file
-    global nc_created
-    nc_created.append(target)
     # check that the values supplied are valid
     # This will convert things to ints, and if it can't be done, throw an error
     if type(year) is not int:
@@ -27,11 +19,9 @@ def set_date(self, year, month, day, base_year = 1900, silent = True):
 
     if type(day) is not int:
         day = float(day)
-    cdo_command = "cdo -setreftime," + str(base_year) + "-01-01 -setdate," + str(year) + "-" + str(month) + "-" + str(day) + " " + self.current + " " + target
+    cdo_command = "cdo -setreftime," + str(base_year) + "-01-01 -setdate," + str(year) + "-" + str(month) + "-" + str(day)
 
-    self.history.append(cdo_command)
-    run_command(cdo_command, self, silent) 
-    if self.run: self.current = target 
+    run_this(cdo_command, self, silent, output = "ensemble")
 
     # clean up the directory
     cleanup(keep = self.current)
