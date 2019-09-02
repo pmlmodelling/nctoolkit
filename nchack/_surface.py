ff09fa03665e69c1a0a917bb3040eefa5b64aaa4
Robert Wilson <robertwilson190@gmail.com>
2019-09-02 16:54:23 +0100

    remove commented out now superfluous

--- nchack/_surface.py
+++ nchack/_surface.py
@@ -8,24 +8,12 @@ from ._cleanup import cleanup
 from ._runthis import run_this
 
 def surface(self, silent = True):
-   # if nc_valid(self.current) == False:
-        #raise ValueError("File is invalid")
-
-   # target = tempfile.NamedTemporaryFile().name + ".nc"
-   # nc_created.append(target)
 
     cdo_command = "cdo -sellevidx,1 "
-    #cdo_command = "cdo --reduce_dim -sellevidx,1 "
-   # run_command(cdo_command, self, silent)
+
     run_this(cdo_command, self, silent, output = "ensemble")
 
-   # self.history.append(cdo_command)
-
-
-   # if self.run: self.current = target
-
     cleanup(keep = self.current)
-
 
     return(self)
 
