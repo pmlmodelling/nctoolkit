fada435c1097872f99c5494f7c96c03989340841
Robert Wilson <robertwilson190@gmail.com>
2019-09-02 17:08:21 +0100

    switch to using run_this

--- nchack/_ensrange.py
+++ nchack/_ensrange.py
@@ -8,27 +8,17 @@ import tempfile
 from .flatten import str_flatten
 from ._filetracker import nc_created
 from ._cleanup import cleanup
-from ._runcommand import run_command
+from ._runthis import run_this
 
 def ensemble_range(self, silent = True):
     """Function to calculate an ensemble range from a list of files"""
-    ff_ensemble = self.current
 
-    if type(ff_ensemble) is not list:
+    if type(self.current) is not list:
         raise ValueError("The current state of the tracker is not a list")
 
-    target = tempfile.NamedTemporaryFile().name + ".nc"
-    owd = os.getcwd()
+    cdo_command = "cdo ensrange " 
 
-   # log the full path of the file
-    global nc_created
-    nc_created.append(target)
-
-    cdo_command = ("cdo ensrange " + str_flatten(ff_ensemble, " ") + " " + target) 
-
-    self.history.append(cdo_command)
-    run_command(cdo_command, self, silent) 
-    if self.run: self.current = target 
+    run_this(cdo_command, self, silent, output = "one")
 
     # clean up the directory
     cleanup(keep = self.current)
