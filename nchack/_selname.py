f1d6eff09744ee152312c3e74128fedb6c42a4c6
Robert Wilson <robertwilson190@gmail.com>
2019-09-02 12:01:52 +0100

    switch to using run_this

--- nchack/_selname.py
+++ nchack/_selname.py
@@ -6,24 +6,19 @@ from .flatten import str_flatten
 from ._cleanup import cleanup
 from ._filetracker import nc_created
 from ._runcommand import run_command
-
+from ._runthis import run_this
 
 def select_variables(self, vars = None, silent = True):
 
-    target  = tempfile.NamedTemporaryFile().name + ".nc"
-    nc_created.append(target)
-    
     if type(vars) is str:
         vars_list = [vars]
     else:
         vars_list = vars
     vars_list = str_flatten(vars_list, ",")
     
-    cdo_command = "cdo selname," + vars_list + " " + self.current + " " + target
-    self.history.append(cdo_command)
-    run_command(cdo_command, self, silent)
-    
-    if self.run: self.current = target 
+    cdo_command = "cdo selname," + vars_list
+
+    run_this(cdo_command, self, silent, output = "ensemble")
     
     cleanup(keep = self.current)
     
