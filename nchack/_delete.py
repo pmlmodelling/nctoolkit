674f7cbcc4505bd8861f001e4437f65b69977e57
Robert Wilson <robertwilson190@gmail.com>
2019-09-02 16:53:04 +0100

    remove runcommand import

--- nchack/_delete.py
+++ nchack/_delete.py
@@ -4,9 +4,7 @@ import tempfile
 from .flatten import str_flatten
 from ._cleanup import cleanup
 from ._filetracker import nc_created
-from ._runcommand import run_command
 from ._runthis import run_this
-
 
 def remove_variable(self, vars, silent = True):
     """Function to select the season"""
