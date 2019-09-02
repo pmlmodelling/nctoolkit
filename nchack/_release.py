b7196026b882fd2cbd7a1db545912af603067d66
Robert Wilson <robertwilson190@gmail.com>
2019-09-02 16:45:49 +0100

    add ability to handle ensembles

--- nchack/_release.py
+++ nchack/_release.py
@@ -5,13 +5,13 @@ import copy
 
 from ._cleanup import cleanup
 from ._filetracker import nc_created
-from ._runcommand import run_command
+from ._runthis import run_this
 
 def release(self, silent = True):
     """Function to release a self from hold mode  """
     # the first step is to set the run status to true
     if self.run:
-        print("Warning: tracker is in run mode. Nothing to release")
+        return("Warning: tracker is in run mode. Nothing to release")
 
     if self.run == False:
         self.run = True
@@ -24,20 +24,10 @@ def release(self, silent = True):
         the_history = copy.deepcopy(self.history)
         
         the_history = the_history[len(pre_history):len(the_history)]
-        
 
         # first, thing to check is whether all of the calls are to cdo
         if len([f for f in the_history if f.startswith("cdo") == False]) > 0:
             raise ValueError("Not all of the calls are to cdo. Exiting!")
-        
-        # we need the start point.
-        # this can be either a file or an ensemble
-        # the end point is irrelevant
-        
-        # First remove anything with a comma
-        start_point = [f for f in the_history[0].split(" ") if "," not in f]
-        start_point = [f for f in start_point if f.endswith(".nc")][0:-1]
-        start_point = " ".join(start_point)
         
         # we need to reverse the history so that the commands are in the correct order for chaining
         the_history.reverse()
@@ -49,7 +39,8 @@ def release(self, silent = True):
         # First, get rid of any mention of cdo
         the_history = the_history.replace("cdo ", "").replace("  ", " ")
         the_history = the_history.split(" ")
-        # Now, we need to remove any files
+        # Now, we need to remove any files from the history
+        # This should probably be removed, as it should not do anything...
         
         the_history = [f for f in the_history if ("," not in f and f.endswith(".nc")) == False]
         the_history = " ".join(the_history)
@@ -59,26 +50,20 @@ def release(self, silent = True):
         for mm in cdo_methods:
             old_history = the_history
             the_history = the_history.replace(" " + mm, " -"+mm)
-        
-        temp_nc = tempfile.NamedTemporaryFile().name + ".nc"
-        nc_created.append(temp_nc)
-        run_this = "cdo " + the_history + " " +  start_point + " " + temp_nc
-        run_this = run_this.replace("  ", " ")
+
+        cdo_command = "cdo " + the_history
+
+        cdo_command = cdo_command.replace("  ", " ")
 
         # OK. We might have reduced dimensions at one point. This needs to be handled.
-        if "--reduce_dim" in run_this:
-            run_this = run_this.replace("--reduce_dim", "")
-            run_this = run_this.replace("cdo","cdo --reduce_dim")
+        if "--reduce_dim" in cdo_command:
+            cdo_command = cdo_command.replace("--reduce_dim", "")
+            cdo_command = cdo_command.replace("cdo","cdo --reduce_dim")
 
-        run_this = run_this.replace("cdo","cdo -L ")
-        run_this = run_this.replace("  ", " ")
-        run_command(run_this, self, silent)
-        # Now we need to modify the history
-        self.history = copy.deepcopy(self.hold_history)
-        self.history.append(run_this)
-        self.current = temp_nc
-
-
+        cdo_command = cdo_command.replace("cdo","cdo -L ")
+        cdo_command = cdo_command.replace("  ", " ")
+        print(cdo_command)
+        run_this(cdo_command, self, silent, output = "ensemble")
 
     return self
     
