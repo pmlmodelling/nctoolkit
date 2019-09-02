3f24ddbd2c394d05c605d280d49197a0e4f5a1b3
Robert Wilson <robertwilson190@gmail.com>
2019-09-02 12:02:01 +0100

    switch to using run_this

--- nchack/_surface.py
+++ nchack/_surface.py
@@ -5,21 +5,23 @@ import itertools
 
 from ._filetracker import nc_created
 from ._cleanup import cleanup 
-from ._runcommand import run_command
+from ._runthis import run_this
 
 def surface(self, silent = True):
    # if nc_valid(self.current) == False:
         #raise ValueError("File is invalid")
 
-    target = tempfile.NamedTemporaryFile().name + ".nc"
-    nc_created.append(target)
+   # target = tempfile.NamedTemporaryFile().name + ".nc"
+   # nc_created.append(target)
 
-    cdo_command = "cdo --reduce_dim -sellevidx,1 " + self.current + " " + target
-    run_command(cdo_command, self, silent)
-    self.history.append(cdo_command)
+    cdo_command = "cdo --reduce_dim -sellevidx,1 "
+   # run_command(cdo_command, self, silent)
+    run_this(cdo_command, self, silent, output = "ensemble")
+
+   # self.history.append(cdo_command)
 
 
-    if self.run: self.current = target
+   # if self.run: self.current = target
 
     cleanup(keep = self.current)
 
