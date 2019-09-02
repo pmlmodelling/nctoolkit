71fc5855d2c714c5306d9e681450cd74e7fc0a2a
Robert Wilson <robertwilson190@gmail.com>
2019-09-02 15:44:22 +0100

    switch to using run_this

--- nchack/_delete.py
+++ nchack/_delete.py
@@ -5,23 +5,18 @@ from .flatten import str_flatten
 from ._cleanup import cleanup
 from ._filetracker import nc_created
 from ._runcommand import run_command
+from ._runthis import run_this
 
 
 def remove_variable(self, vars, silent = True):
     """Function to select the season"""
 
-    target  = tempfile.NamedTemporaryFile().name + ".nc"
-    nc_created.append(target)
-
     if type(vars) is not list:
         vars = [vars]
     vars = str_flatten(vars, ",")
     
-    cdo_command = "cdo delete,name=" + vars + " " + self.current + " " + target
-    self.history.append(cdo_command)
-    run_command(cdo_command, self, silent)
-    
-    if self.run: self.current = target 
+    cdo_command = "cdo delete,name=" + vars
+    run_this(cdo_command, self, silent, output = "ensemble")
     
     cleanup(keep = self.current)
     
