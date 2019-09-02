4e965c23a600dff8db96dbdb2ca3434324f0280f
Robert Wilson <robertwilson190@gmail.com>
2019-09-02 16:57:39 +0100

    switch to using run_this

--- nchack/_cdo_command.py
+++ nchack/_cdo_command.py
@@ -10,24 +10,19 @@ from ._clip import clip
 from ._filetracker import nc_created
 from ._cleanup import cleanup 
 from .flatten import str_flatten 
-from ._runcommand import run_command
+from ._runthis import run_this
 
 def cdo_command(self, command, silent = True):
     """ Function to all any cdo command of the the form 'command + infile + outfile'"""
 
-    target = tempfile.NamedTemporaryFile().name + ".nc"
-    nc_created.append(target)
     if type(self.current) == list:
         infile = str_flatten(self.current, " ")
     else:
         infile = self.current
 
-    cdo_command = "cdo " + command + " " + infile + " " + target
+    cdo_command = "cdo " + command
 
-    self.history.append(cdo_command)
-    run_command(cdo_command, self, silent)
-
-    if self.run: self.current = target
+    run_this(cdo_command, self, silent, output = "ensemble")
 
     cleanup(keep = self.current)
 
