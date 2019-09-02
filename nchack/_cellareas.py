431ffd947cc9bd1ef3fbca81b3257c25b827128a
Robert Wilson <robertwilson190@gmail.com>
2019-09-02 16:56:25 +0100

    switch to using run_this

--- nchack/_cellareas.py
+++ nchack/_cellareas.py
@@ -3,16 +3,13 @@ import tempfile
 
 from ._cleanup import cleanup
 from ._filetracker import nc_created
-from ._runcommand import run_command
+from ._runthis import run_this
 
 def cell_areas(self, silent = True):
-    target = tempfile.NamedTemporaryFile().name + ".nc"
-    nc_created.append(target)
-    cdo_command = ( "cdo gridarea " + self.current + " " + target)
-    self.history.append(cdo_command)
-    run_command(cdo_command, self, silent)
 
-    if self.run: self.current = target
+    cdo_command = "cdo gridarea "
+    run_this(cdo_command, self, silent, output = "ensemble")
+
     cleanup(keep = self.current)
     
     return(self)
