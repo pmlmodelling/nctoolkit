57db4c566ddbf88b2402c0490cf638e0bf6e430d
Robert Wilson <robertwilson190@gmail.com>
2019-09-02 16:32:00 +0100

    switch to using run_this

--- nchack/_vertint.py
+++ nchack/_vertint.py
@@ -17,27 +17,27 @@ def vertical_interp(self, vert_depths = None, silent = True):
        
     # first a quick fix for the case when there is only one vertical depth
 
-    if vert_depths != None:
-        if (type(vert_depths) == int) or (type(vert_depths) == float):
-            vert_depths = {vert_depths}
+  #  if vert_depths != None:
+  #      if (type(vert_depths) == int) or (type(vert_depths) == float):
+  #          vert_depths = {vert_depths}
 
-    if vert_depths == None:
-        vertical_remap = False
-    
-    if vert_depths != None:
-        num_depths = len(self.depths())
-        if num_depths < 2:
-            print("There are none or one vertical depths in the file. Vertical interpolation not carried out.")
-            vertical_remap = False
-    if ((vert_depths != None) and vertical_remap):
-        available_depths = self.depths() 
+  #  if vert_depths == None:
+  #      vertical_remap = False
+  #  
+  #  if vert_depths != None:
+  #      num_depths = len(self.depths())
+  #      if num_depths < 2:
+  #          print("There are none or one vertical depths in the file. Vertical interpolation not carried out.")
+  #          vertical_remap = False
+  #  if ((vert_depths != None) and vertical_remap):
+  #      available_depths = self.depths() 
     
     # Check if min/max depths are outside valid ranges. This should possibly be a warning, not error
     if vertical_remap:
-        if (min(vert_depths) < min(available_depths)):
-             raise ValueError("error:minimum depth supplied is too low")
-        if (max(vert_depths) > max(available_depths)):
-             raise ValueError("error: maximum depth supplied is too low")
+   #     if (min(vert_depths) < min(available_depths)):
+   #          raise ValueError("error:minimum depth supplied is too low")
+   #     if (max(vert_depths) > max(available_depths)):
+   #          raise ValueError("error: maximum depth supplied is too low")
 
         vert_depths = str_flatten(vert_depths, ",")
         cdo_command = "cdo intlevel," + vert_depths
