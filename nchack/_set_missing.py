9236c1fde52e19d864dcf9405b480423bddfe4e0
Robert Wilson <robertwilson190@gmail.com>
2019-09-02 17:13:46 +0100

    switching to using run_this

--- nchack/_set_missing.py
+++ nchack/_set_missing.py
@@ -6,31 +6,21 @@ from .flatten import str_flatten
 from ._variables import variables
 from ._filetracker import nc_created
 from ._cleanup import cleanup
-from ._runcommand import run_command
+from ._runthis import run_this
 
 def set_missing(self, value, silent = True):
     """Function to set the missing values"""
     """This is either a range or a single value"""
 
-    ff = self.current
-
-    target = tempfile.NamedTemporaryFile().name + ".nc"
-    owd = os.getcwd()
-   # log the full path of the file
-    global nc_created
-    nc_created.append(target)
     if type(value) is int:
         value = float(value)
 
     if type(value) is float:
-        cdo_command = ("cdo setctomiss," + str(value) + " " +  ff + " " + target) 
+        cdo_command = "cdo setctomiss," + str(value)
     if type(value) is list:
-        cdo_command = ("cdo setrtomiss," + str(value[0]) + "," + str(value[1]) + " " +  ff + " " + target) 
+        cdo_command = "cdo setrtomiss," + str(value[0]) + "," + str(value[1])
 
-
-    self.history.append(cdo_command)
-    run_command(cdo_command, self, silent) 
-    if self.run: self.current = target 
+    run_this(cdo_command, self, silent, output = "ensemble")
 
     # clean up the directory
     cleanup(keep = self.current)
