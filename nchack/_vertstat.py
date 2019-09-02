1eff493e213d54b4a0875550215e46d032b23393
Robert Wilson <robertwilson190@gmail.com>
2019-09-02 16:32:08 +0100

    switch to using run_this

--- nchack/_vertstat.py
+++ nchack/_vertstat.py
@@ -4,22 +4,13 @@ import tempfile
 
 from ._filetracker import nc_created
 from ._cleanup import cleanup
-from ._runcommand import run_command
+from ._runthis import run_this
 
 def vertstat(self, stat = "mean", silent = True):
     """Function to calculate the vertical mean from a function""" 
-    ff = self.current
+    cdo_command = "cdo --reduce_dim -vert" + stat
 
-    target = tempfile.NamedTemporaryFile().name + ".nc"
-
-    global nc_created
-    nc_created.append(target)
-
-    cdo_command = ("cdo --reduce_dim -vert" + stat + " " + ff + " " + target) 
-
-    self.history.append(cdo_command)
-    run_command(cdo_command, self, silent) 
-    if self.run: self.current = target 
+    run_this(cdo_command, self, silent, output = "ensemble")
 
     # clean up the directory
     cleanup(keep = self.current)
