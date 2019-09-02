2ac6bc2362f22c9983404732c9befba956663237
Robert Wilson <robertwilson190@gmail.com>
2019-09-02 17:03:40 +0100

    switch to using run_this

--- nchack/_ensemble_percentile.py
+++ nchack/_ensemble_percentile.py
@@ -7,27 +7,17 @@ import tempfile
 from .flatten import str_flatten
 from ._filetracker import nc_created
 from ._cleanup import cleanup
-from ._runcommand import run_command
+from ._runthis import run_this
 
 def ensemble_percentile(self, p = 50, silent = True):
     """Function to calculate an ensemble percentile from a list of files"""
-    ff_ensemble = self.current
 
-    if type(ff_ensemble) is not list:
+    if type(self.current) is not list:
         raise ValueError("The current state of the tracker is not a list")
 
-    target = tempfile.NamedTemporaryFile().name + ".nc"
-    owd = os.getcwd()
+    cdo_command = "cdo enspctl," + str(p)
 
-   # log the full path of the file
-    global nc_created
-    nc_created.append(target)
-
-    cdo_command = ("cdo enspctl," + str(p) + " " + str_flatten(ff_ensemble, " ") + " " + target) 
-
-    self.history.append(cdo_command)
-    run_command(cdo_command, self, silent) 
-    if self.run: self.current = target 
+    run_this(cdo_command, self, silent, output = "one")
 
     # clean up the directory
     cleanup(keep = self.current)
