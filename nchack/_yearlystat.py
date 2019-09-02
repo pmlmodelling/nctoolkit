ef586d0a853d8c35c23e72fafd0b3280888d247c
Robert Wilson <robertwilson190@gmail.com>
2019-09-02 16:35:16 +0100

    switch to using run_this

--- nchack/_yearlystat.py
+++ nchack/_yearlystat.py
@@ -6,22 +6,14 @@ from .flatten import str_flatten
 from ._filetracker import nc_created
 from ._cleanup import cleanup
 
-from ._runcommand import run_command
+from ._runthis import run_this
 
 def yearlystat(self, stat = "mean", silent = True):
     """Function to calculate the seasonal statistic from a function""" 
-    ff = self.current
 
-    target = tempfile.NamedTemporaryFile().name + ".nc"
+    cdo_command = "cdo -year" + stat
 
-    global nc_created
-    nc_created.append(target)
-
-    cdo_command = ("cdo -year" + stat + " " + ff + " " + target) 
-
-    self.history.append(cdo_command)
-    run_command(cdo_command, self, silent) 
-    if self.run: self.current = target 
+    run_this(cdo_command, self, silent, output = "ensemble")
 
     # clean up the directory
     cleanup(keep = self.current)
