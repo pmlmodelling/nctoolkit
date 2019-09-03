a01d2fb3d75bde94e9216afd94094cf721bd02a9
Robert Wilson <robertwilson190@gmail.com>
2019-09-03 16:11:59 +0100

    add cores option

--- nchack/_time_stat.py
+++ nchack/_time_stat.py
@@ -7,13 +7,12 @@ from ._filetracker import nc_created
 from ._cleanup import cleanup
 from ._runthis import run_this
 
-def time_stat(self, stat = "mean", silent = True):
+def time_stat(self, stat = "mean", silent = True, cores = 1):
     """Function to calculate the mean from from a single file"""
 
     cdo_command = "cdo tim" + stat
 
-   # run_command(cdo_command, self, silent) 
-    run_this(cdo_command, self, silent, output = "ensemble")
+    run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
 
     # clean up the directory
     cleanup(keep = self.current)
@@ -21,20 +20,20 @@ def time_stat(self, stat = "mean", silent = True):
     return(self)
     
 
-def time_mean(self, silent = True):
-    return(time_stat(self, stat = "mean", silent = silent))
+def time_mean(self, silent = True, cores = 1):
+    return time_stat(self, stat = "mean", silent = silent, cores = cores)
 
-def time_min(self, silent = True):
-    return(time_stat(self, stat = "min", silent = silent))
+def time_min(self, silent = True, cores = 1):
+    return time_stat(self, stat = "min", silent = silent, cores = cores)
 
-def time_max(self, silent = True):
-    return(time_stat(self, stat = "max", silent = silent))
+def time_max(self, silent = True, cores = 1):
+    return time_stat(self, stat = "max", silent = silent, cores = cores)
 
-def time_range(self, silent = True):
-    return(time_stat(self,jstat = "range", silent = silent))
+def time_range(self, silent = True, cores = 1):
+    return time_stat(self,stat = "range", silent = silent, cores = cores)
 
-def time_var(self, silent = True):
-    return(time_stat(self, stat = "var", silent = silent))
+def time_var(self, silent = True, cores = 1):
+    return time_stat(self, stat = "var", silent = silent, cores = cores)
 
 
 
