61d9e1d838dbadd08349ea50d39a20145551895d
Robert Wilson <robertwilson190@gmail.com>
2019-09-03 16:03:35 +0100

    add cores option

--- nchack/_yearlystat.py
+++ nchack/_yearlystat.py
@@ -8,12 +8,12 @@ from ._cleanup import cleanup
 
 from ._runthis import run_this
 
-def yearlystat(self, stat = "mean", silent = True):
+def yearlystat(self, stat = "mean", silent = True, cores = 1):
     """Function to calculate the seasonal statistic from a function""" 
 
     cdo_command = "cdo -year" + stat
 
-    run_this(cdo_command, self, silent, output = "ensemble")
+    run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
 
     # clean up the directory
     cleanup(keep = self.current)
@@ -21,14 +21,14 @@ def yearlystat(self, stat = "mean", silent = True):
     return(self)
     
 
-def yearly_mean(self, silent = True):
-    return yearlystat(self, stat = "mean", silent = True)
+def yearly_mean(self, silent = True, cores = 1):
+    return yearlystat(self, stat = "mean", silent = True, cores = cores)
 
-def yearly_min(self, silent = True):
-    return yearlystat(self, stat = "min", silent = True)
+def yearly_min(self, silent = True, cores = 1):
+    return yearlystat(self, stat = "min", silent = True, cores = cores)
 
-def yearly_max(self, silent = True):
-    return yearlystat(self, stat = "max", silent = True)
+def yearly_max(self, silent = True, cores = 1):
+    return yearlystat(self, stat = "max", silent = True, cores = cores)
     
-def yearly_range(self, silent = True):
-    return yearlystat(self, stat = "range", silent = True)
+def yearly_range(self, silent = True, cores = 1):
+    return yearlystat(self, stat = "range", silent = True, cores = cores)
