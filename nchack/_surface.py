ee2430da557c76fbc6cfcf3f92665e1f3ab8e929
Robert Wilson <robertwilson190@gmail.com>
2019-09-03 15:33:28 +0100

    add cores option

--- nchack/_surface.py
+++ nchack/_surface.py
@@ -7,11 +7,11 @@ from ._filetracker import nc_created
 from ._cleanup import cleanup 
 from ._runthis import run_this
 
-def surface(self, silent = True):
+def surface(self, silent = True, cores = 1):
 
     cdo_command = "cdo -sellevidx,1 "
 
-    run_this(cdo_command, self, silent, output = "ensemble")
+    run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
 
     cleanup(keep = self.current)
 
