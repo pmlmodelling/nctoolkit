1851448219a69e5f8542c743dc9570ce1823dda4
Robert Wilson <robertwilson190@gmail.com>
2019-09-03 16:06:42 +0100

    add cores option

--- nchack/_monthlyclim.py
+++ nchack/_monthlyclim.py
@@ -7,13 +7,13 @@ from ._filetracker import nc_created
 from ._cleanup import cleanup
 from ._runthis import run_this
 
-def ymonstat(self, stat = "mean", silent = True):
+def ymonstat(self, stat = "mean", silent = True, cores = 1):
     """Function to calculate the seasonal statistic from a function""" 
 
     cdo_command = "cdo -ymon" + stat
 
     self.history.append(cdo_command)
-    run_this(cdo_command, self, silent, output = "ensemble")
+    run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
 
     # clean up the directory
     cleanup(keep = self.current)
@@ -21,14 +21,14 @@ def ymonstat(self, stat = "mean", silent = True):
     return(self)
     
 
-def monthly_mean_climatology(self, silent = True):
-    return ymonstat(self, stat = "mean", silent = True)
+def monthly_mean_climatology(self, silent = True, cores = 1):
+    return ymonstat(self, stat = "mean", silent = True, cores = cores)
 
-def monthly_min_climatology(self, silent = True):
-    return ymonstat(self, stat = "min", silent = True)
+def monthly_min_climatology(self, silent = True, cores = 1):
+    return ymonstat(self, stat = "min", silent = True, cores = cores)
 
-def monthly_max_climatology(self, silent = True):
-    return ymonstat(self,  stat = "max", silent = True)
+def monthly_max_climatology(self, silent = True, cores = 1):
+    return ymonstat(self,  stat = "max", silent = True, cores = cores)
     
-def monthly_range_climatology(self, silent = True):
-    return ymonstat(self, stat = "range", silent = True)
+def monthly_range_climatology(self, silent = True, cores = 1):
+    return ymonstat(self, stat = "range", silent = True, cores = cores)
