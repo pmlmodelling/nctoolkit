581b5e8e531020f7332657ab8b1e59542a5e497b
Robert Wilson <robertwilson190@gmail.com>
2019-09-03 16:29:57 +0100

    fix unnecessary addition to history etc.

--- nchack/_mergetime.py
+++ nchack/_mergetime.py
@@ -12,20 +12,14 @@ from ._runthis import run_this
 
 def merge_time(self, silent = True):
     """Function to calculate an ensemble range from a list of files"""
-    ff_ensemble = self.current
 
-    if type(ff_ensemble) is not list:
+    if type(self.current) is not list:
         raise ValueError("The current state of the tracker is not a list")
 
-    target = tempfile.NamedTemporaryFile().name + ".nc"
-
    # log the full path of the file
-    global nc_created
-    nc_created.append(target)
 
     cdo_command = ("cdo mergetime ")
 
-    self.history.append(cdo_command)
     run_this(cdo_command, self, silent, output = "one") 
 
     # clean up the directory
