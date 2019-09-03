3889b2b822fead508c818bc2c1090d793a2b6f5b
Robert Wilson <robertwilson190@gmail.com>
2019-09-03 16:18:57 +0100

    add cores option

--- nchack/_cdo_command.py
+++ nchack/_cdo_command.py
@@ -12,7 +12,7 @@ from ._cleanup import cleanup
 from .flatten import str_flatten 
 from ._runthis import run_this
 
-def cdo_command(self, command, silent = True):
+def cdo_command(self, command, silent = True, cores = 1):
     """ Function to all any cdo command of the the form 'command + infile + outfile'"""
 
     if type(self.current) == list:
@@ -22,7 +22,7 @@ def cdo_command(self, command, silent = True):
 
     cdo_command = "cdo " + command
 
-    run_this(cdo_command, self, silent, output = "ensemble")
+    run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
 
     cleanup(keep = self.current)
 
