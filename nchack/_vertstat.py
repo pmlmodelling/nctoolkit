fe224128f6a81dba7dae92488424944f679798d8
Robert Wilson <robertwilson190@gmail.com>
2019-09-03 16:17:48 +0100

    add cores option

--- nchack/_vertstat.py
+++ nchack/_vertstat.py
@@ -6,11 +6,11 @@ from ._filetracker import nc_created
 from ._cleanup import cleanup
 from ._runthis import run_this
 
-def vertstat(self, stat = "mean", silent = True):
+def vertstat(self, stat = "mean", silent = True, cores = 1):
     """Function to calculate the vertical mean from a function""" 
     cdo_command = "cdo --reduce_dim -vert" + stat
 
-    run_this(cdo_command, self, silent, output = "ensemble")
+    run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
 
     # clean up the directory
     cleanup(keep = self.current)
@@ -18,14 +18,14 @@ def vertstat(self, stat = "mean", silent = True):
     return(self)
     
 
-def vertical_mean(self, silent = True):
-    return vertstat(self, stat = "mean", silent = True)
+def vertical_mean(self, silent = True, cores = 1):
+    return vertstat(self, stat = "mean", silent = True, cores = cores)
 
-def vertical_min(self, silent = True):
-    return vertstat(self, stat = "min", silent = True)
+def vertical_min(self, silent = True, cores = 1):
+    return vertstat(self, stat = "min", silent = True, cores = cores)
 
-def vertical_max(self, silent = True):
-    return vertstat(self, stat = "max", silent = True)
+def vertical_max(self, silent = True, cores = 1):
+    return vertstat(self, stat = "max", silent = True, cores = cores)
     
-def vertical_range(self, silent = True):
-    return vertstat(self, stat = "range", silent = True)
+def vertical_range(self, silent = True, cores = 1):
+    return vertstat(self, stat = "range", silent = True, cores = cores)
