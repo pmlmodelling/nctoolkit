4d6a43ac07b3e8250f013eeb7563481d75cfc432
Robert Wilson <robertwilson190@gmail.com>
2019-09-04 11:30:11 +0100

    add merge method

---
+++ nchack/_merge.py
@@ -0,0 +1,26 @@
+
+import os
+import tempfile
+
+from .flatten import str_flatten
+from ._filetracker import nc_created
+from ._cleanup import cleanup
+from ._runthis import run_this
+
+def merge(self, silent = True):
+    """Function to merge a list of files"""
+
+    if type(self.current) is not list:
+        raise ValueError("The current state of the tracker is not a list")
+
+   # log the full path of the file
+
+    cdo_command = ("cdo merge")
+
+    run_this(cdo_command, self, silent, output = "one") 
+
+    # clean up the directory
+    cleanup(keep = self.current)
+
+    return(self)
+    
