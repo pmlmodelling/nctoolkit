0529dd01cf98167698465dfa997157352cba1dc4
Robert Wilson <robertwilson190@gmail.com>
2019-09-05 09:56:45 +0100

    add rolling methods

---
+++ nchack/_rollstat.py
@@ -0,0 +1,37 @@
+
+import os
+import tempfile
+
+from .flatten import str_flatten
+from ._filetracker import nc_created
+from ._cleanup import cleanup
+from ._runthis import run_this
+
+def rollstat(self, window,  stat = "mean", silent = True, cores = 1):
+    """Function to calculate the monthly statistic from a netcdf file""" 
+    
+    cdo_command = "cdo -run" + stat + "," + str(window)
+
+    run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
+
+    # clean up the directory
+    cleanup(keep = self.current)
+
+    return(self)
+    
+
+def rolling_mean(self, window, silent = True, cores = 1):
+    return rollstat(self, window = window, stat = "mean", silent = silent, cores = cores)
+
+def rolling_min(self, window, silent = True, cores = 1):
+    return rollstat(self, window = window, stat = "min", silent = silent, cores = cores)
+
+def rolling_max(self, window, silent = True, cores = 1):
+    return rollstat(self, window = window, stat = "max", silent = silent, cores = cores)
+    
+def rolling_range(self, window, silent = True, cores = 1):
+    return rollstat(self, window = window, stat = "range", silent = silent, cores = cores)
+
+def rolling_sum(self, window, silent = True, cores = 1):
+    return rollstat(self, window = window, stat = "sum", silent = silent, cores = cores)
+
