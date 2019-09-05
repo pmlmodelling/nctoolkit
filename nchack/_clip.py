7675cb872ddaa9fb97598fb3b613f9e112fb4a10
Robert Wilson <robertwilson190@gmail.com>
2019-09-05 17:00:06 +0100

    rename lon_range lon and lat_range lat

--- nchack/_clip.py
+++ nchack/_clip.py
@@ -6,28 +6,28 @@ from .flatten import str_flatten
 from ._filetracker import nc_created
 from ._cleanup import cleanup
 
-def clip(self, lon_range = [-180, 180], lat_range = [-90, 90], silent = True, cores = 1):
+def clip(self, lon = [-180, 180], lat = [-90, 90], silent = True, cores = 1):
     """ Function to clip netcdf files, spatially"""
-    if (type(lon_range) is not list) or (type(lat_range) is not list):
+    if (type(lon) is not list) or (type(lat) is not list):
         raise ValueError("Check that lon/lat ranges are tuples")
     
-    if(type(lon_range[0]) is float ) or ( type(lon_range[0]) is int) == False:
-        raise ValueError("Check lon_range")
+    if(type(lon[0]) is float ) or ( type(lon[0]) is int) == False:
+        raise ValueError("Check lon")
     
-    if( type(lon_range[1]) is float ) or ( type(lon_range[1]) is int) == False:
-        raise ValueError("Check lon_range")
+    if( type(lon[1]) is float ) or ( type(lon[1]) is int) == False:
+        raise ValueError("Check lon")
 
-    if( type(lat_range[0]) is float ) or ( type(lat_range[0]) is int) == False:
-        raise ValueError("Check lat_range")
+    if( type(lat[0]) is float ) or ( type(lat[0]) is int) == False:
+        raise ValueError("Check lat")
     
-    if( type(lat_range[1]) is float ) or ( type(lat_range[1]) is int) == False:
-        raise ValueError("Check lat_range")
+    if( type(lat[1]) is float ) or ( type(lat[1]) is int) == False:
+        raise ValueError("Check lat")
 
     # now, clip to the lonlat box we need
 
-    if lon_range[0] > -180 or lon_range[1] > 180 or lat_range[0] > -90 or lat_range[1] < 90:
+    if lon[0] > -180 or lon[1] > 180 or lat[0] > -90 or lat[1] < 90:
 
-        lat_box = str_flatten(lon_range + lat_range)
+        lat_box = str_flatten(lon + lat)
         cdo_command = ("cdo sellonlatbox," + lat_box)
         run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
 
