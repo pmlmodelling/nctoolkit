19261005e336c35818e79802103205e03e8fbb49
Robert Wilson <robertwilson190@gmail.com>
2019-09-05 17:00:30 +0100

    add ability to read ensembles

--- nchack/_toxarray.py
+++ nchack/_toxarray.py
@@ -7,8 +7,11 @@ from ._cleanup import cleanup
 
 def to_xarray(self, decode_times = True):
     """convert tracker to xarray data set"""
-    ff = self.current
-    data = xr.open_dataset(ff, decode_times = decode_times)
+    
+    if type(self.current) is str:
+        data = xr.open_dataset(self.current, decode_times = decode_times)
+    else:   
+        data = xr.open_mfdataset(self.current, decode_times = decode_times)
 
     cleanup(keep = self.current)
     return(data)
