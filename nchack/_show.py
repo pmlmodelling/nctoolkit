708ff581adf8aa322394e721ff7e73a637786623
Robert Wilson <robertwilson190@gmail.com>
2019-09-06 16:48:06 +0100

    remove redundant imports

--- nchack/_show.py
+++ nchack/_show.py
@@ -1,10 +1,5 @@
-
 import os
-import tempfile
-
 from ._cleanup import cleanup
-from ._filetracker import nc_created
-from ._runcommand import run_command
 
 def show_years(self):
     cdo_result = os.popen( "cdo showyear " + self.current).read()
@@ -14,7 +9,6 @@ def show_years(self):
     cdo_result =  [int(v) for v in cdo_result]
     cdo_result.sort()
     return(cdo_result)
-
 
 def show_months(self):
     cdo_result = os.popen( "cdo showmon " + self.current).read()
