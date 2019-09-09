d4e368b86e040d1e9234fb356a53a93de40b1301
Robert Wilson <robertwilson190@gmail.com>
2019-09-09 09:41:28 +0100

    add check for valid lonlat ranges

--- nchack/_clip.py
+++ nchack/_clip.py
@@ -1,11 +1,9 @@
-
-
 from ._runthis import run_this
 from .flatten import str_flatten
 from ._cleanup import cleanup
 
 def clip(self, lon = [-180, 180], lat = [-90, 90], silent = True, cores = 1):
-    """ Function to clip netcdf files, spatially"""
+    """Method to clip netcdf files, spatially"""
     if (type(lon) is not list) or (type(lat) is not list):
         raise ValueError("Check that lon/lat ranges are tuples")
     
@@ -23,11 +21,13 @@ def clip(self, lon = [-180, 180], lat = [-90, 90], silent = True, cores = 1):
 
     # now, clip to the lonlat box we need
 
-    if lon[0] > -180 or lon[1] > 180 or lat[0] > -90 or lat[1] < 90:
+    if lon[0] >= -180 and lon[1] <= 180 and lat[0] >= -90 and lat[1] <= 90:
 
         lat_box = str_flatten(lon + lat)
         cdo_command = ("cdo sellonlatbox," + lat_box)
         run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
+    else:
+        raise ValueError("The lonlat box supplied is not valid!")
 
     # clean up the directory
     cleanup(keep = self.current)
