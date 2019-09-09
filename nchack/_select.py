f7570b4daa7fe487f370b24c8d4deae30497882a
Robert Wilson <robertwilson190@gmail.com>
2019-09-09 10:30:50 +0100

    move select_variables to _select.py

--- nchack/_select.py
+++ nchack/_select.py
@@ -2,6 +2,8 @@
 from .flatten import str_flatten
 from ._cleanup import cleanup
 from ._runthis import run_this
+from ._variables import variables
+
 
 def select_season(self, season, silent = True, cores = 1):
     """Method to select the season"""
@@ -47,6 +49,31 @@ def select_years(self, years, silent = True, cores = 1):
     years = str_flatten(years, ",") 
 
     cdo_command = "cdo selyear," + years
+    run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
+    
+    cleanup(keep = self.current)
+    
+    return self
+
+
+def select_variables(self, vars = None, silent = True, cores = 1):
+    """Method to select variables from a netcdf file"""
+
+    if type(vars) is str:
+        vars_list = [vars]
+    else:
+        vars_list = vars
+
+    valid_vars = self.variables()
+
+    for vv in vars_list:
+        if vv not in valid_vars:
+            raise ValueError(vv + " is not available in the file!")
+
+    vars_list = str_flatten(vars_list, ",")
+    
+    cdo_command = "cdo selname," + vars_list
+
     run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
     
     cleanup(keep = self.current)
