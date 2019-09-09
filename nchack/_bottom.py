e22c302e45b68fe1d5e638de5a38b8a4a6d6150b
Robert Wilson <robertwilson190@gmail.com>
2019-09-09 10:02:03 +0100

    make return syntax consistent throughout package

--- nchack/_bottom.py
+++ nchack/_bottom.py
@@ -1,4 +1,5 @@
-
+# Todo:
+# add a way to make sure this always works in hold mode. i.e. if we do vertical interpolation
 import os
 
 from ._cleanup import cleanup 
@@ -7,22 +8,19 @@ from ._runthis import run_this
 def bottom(self, silent = True, cores = 1):
     """Method to extract the bottom level from netcdf files"""
 
+    # extract the number of the bottom level
+    # Use the first file for an ensemble
+    # pull the cdo command together, then run it or store it
     if type(self.current) is list:
         ff = self.current[0]
         print("warning: first file in ensemble used to determine number of vertical levels")
     else:
         ff = self.current
-
     n_levels = int(os.popen( "cdo nlevel " + ff).read().split("\n")[0])
-    
-    # First, we need to get the number of levels 
     cdo_command = "cdo -sellevidx," + str(n_levels)
 
     run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
 
     cleanup(keep = self.current)
 
-    return(self)
-
-
-
+    return self
