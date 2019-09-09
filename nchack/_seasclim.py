a2fa71b85c22199a4ff6e9c13a486e5aea449f20
Robert Wilson <robertwilson190@gmail.com>
2019-09-09 10:10:07 +0100

    make return syntax consistent throughout package

--- nchack/_seasclim.py
+++ nchack/_seasclim.py
@@ -3,7 +3,7 @@ from ._cleanup import cleanup
 from ._runthis import run_this
 
 def seasstat(self, stat = "mean", silent = True, cores = 1):
-    """Function to calculate the seasonal statistic from a function""" 
+    """Method to calculate the seasonal statistic from a function""" 
     cdo_command = "cdo -yseas" + stat 
 
     run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
@@ -11,7 +11,7 @@ def seasstat(self, stat = "mean", silent = True, cores = 1):
     # clean up the directory
     cleanup(keep = self.current)
 
-    return(self)
+    return self
     
 
 def seasonal_mean_climatology(self, silent = True, cores = 1):
