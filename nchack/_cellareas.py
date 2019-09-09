9a69a7154870682c5026ddc52fb4845beafd5b80
Robert Wilson <robertwilson190@gmail.com>
2019-09-09 15:23:08 +0100

    make return synthax consistent throughout package

--- nchack/_cellareas.py
+++ nchack/_cellareas.py
@@ -1,14 +1,8 @@
-
-
 from ._cleanup import cleanup
 from ._runthis import run_this
 
 def cell_areas(self, silent = True):
-
     cdo_command = "cdo gridarea "
     run_this(cdo_command, self, silent, output = "ensemble")
-
     cleanup(keep = self.current)
-    
-    return(self)
-
+    return self
