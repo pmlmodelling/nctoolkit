0620789f8a2286d2bebf3811b57f8d690394cc75
Robert Wilson <robertwilson190@gmail.com>
2019-09-09 10:06:56 +0100

    make return syntax consistent throughout package

--- nchack/_mergetime.py
+++ nchack/_mergetime.py
@@ -6,19 +6,19 @@ from ._cleanup import cleanup
 from ._runthis import run_this
 
 def merge_time(self, silent = True):
-    """Function to calculate an ensemble range from a list of files"""
+    """Method to calculate an ensemble range from a list of files"""
 
     if type(self.current) is not list:
         raise ValueError("The current state of the tracker is not a list")
 
    # log the full path of the file
 
-    cdo_command = ("cdo mergetime ")
+    cdo_command = "cdo mergetime "
 
     run_this(cdo_command, self, silent, output = "one") 
 
     # clean up the directory
     cleanup(keep = self.current)
 
-    return(self)
+    return self
     
