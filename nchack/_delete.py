a87a0a20b509fea40888544c4fad7d9f84a46514
Robert Wilson <robertwilson190@gmail.com>
2019-09-09 09:49:00 +0100

    add check that variable exists

--- nchack/_delete.py
+++ nchack/_delete.py
@@ -2,12 +2,20 @@
 from .flatten import str_flatten
 from ._cleanup import cleanup
 from ._runthis import run_this
+from ._variables import variables
 
 def remove_variable(self, vars, silent = True, cores = 1):
     """Function to select the season"""
 
     if type(vars) is not list:
         vars = [vars]
+
+    actual_vars = self.variables()
+    
+    for vv in vars:
+        if vv not in actual_vars:
+            raise ValueError("Variable " + vv + " is not in the file!")
+
     vars = str_flatten(vars, ",")
     
     cdo_command = "cdo delete,name=" + vars
