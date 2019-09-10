2b02eb91f51cdba6748a5ea3fc51ac48ad027f1e
Robert Wilson <robertwilson190@gmail.com>
2019-09-10 11:28:57 +0100

    remove variables imports

--- nchack/_delete.py
+++ nchack/_delete.py
@@ -2,25 +2,12 @@
 from .flatten import str_flatten
 from ._cleanup import cleanup
 from ._runthis import run_this
-from ._variables import variables
-from ._variables import nc_variables
 
 def remove_variable(self, vars, silent = True, cores = 1):
-    """Function to select the season"""
+    """Method to remove sellected variables from tracker"""
 
     if type(vars) is not list:
         vars = [vars]
-
-   # if type(self.current) is str:
-   #     file_list = [self.current]
-   # else:
-   #     file_list = self.current
-  
-   # for ff in file_list:
-   #     valid_vars = nc_variables(ff)
-   #     for vv in vars:
-   #         if vv not in valid_vars:
-   #             raise ValueError(vv + " is not available in " + ff)
 
     vars = str_flatten(vars, ",")
     
