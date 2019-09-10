70e4390342a03193037b806e194b9374a97b5349
Robert Wilson <robertwilson190@gmail.com>
2019-09-10 11:38:08 +0100

    delete times after move to show

--- nchack/_times.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def times(self):
-    ff = self.current
-    cdo_result = os.popen( "cdo showtimestamp " + ff).read()
-    cdo_result = cdo_result.replace("\n", "")
-    cdo_result = cdo_result.split()
-    cdo_result = pd.Series( (v for v in cdo_result) )
-    #return cdo_result
