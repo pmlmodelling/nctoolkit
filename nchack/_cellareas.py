af706091a3aa7e89184e6bc88baa8b459885c9a6
Robert Wilson <robertwilson190@gmail.com>
2019-09-12 11:46:46 +0100

    add cores option to method

--- nchack/_cellareas.py
+++ nchack/_cellareas.py
@@ -3,6 +3,6 @@ from ._runthis import run_this
 
 def cell_areas(self, silent = True, cores = 1):
     cdo_command = "cdo gridarea "
-    run_this(cdo_command, self, silent, output = "ensemble", cores)
+    run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
     cleanup(keep = self.current)
     #return self
