2286bbe5f3522bab8e19b0af693abbb07bb9c1f4
Robert Wilson <robertwilson190@gmail.com>
2019-09-13 09:02:29 +0100

    add variable option

--- nchack/_ens_nco.py
+++ nchack/_ens_nco.py
@@ -7,21 +7,32 @@ from ._filetracker import nc_created
 from ._cleanup import cleanup
 from ._runcommand import run_command
 
-def ensemble_nco(self, method, silent = True):
+def ensemble_nco(self, method, vars = None, silent = True):
     """Method to calculate an ensemble stat from a list of files"""
     if self.release == False:
         raise ValueError("NCO methods cannot be held over")
 
     ff_ensemble = self.current
+
     if type(ff_ensemble) is not list:
         raise ValueError("The current state of the tracker is not a list")
+
+    if vars is not None:
+        if type(vars) == str:
+            vars = [vars]
+
+        if type(vars) is not list:
+            raise ValueError("vars supplied is not a list or str!")
+
     target = tempfile.NamedTemporaryFile().name + ".nc"
-    owd = os.getcwd()
-   # log the full path of the file
+
     global nc_created
     nc_created.append(target)
-
-    nco_command = ("ncea -y " + method + " " + str_flatten(ff_ensemble, " ") + " " + target) 
+    
+    if vars is None:
+        nco_command = ("ncea -y " + method + " " + str_flatten(ff_ensemble, " ") + " " + target) 
+    else:
+        nco_command = ("ncea -y " + method + " -v " + str_flatten(vars, ",") + " " + str_flatten(ff_ensemble, " ") + " " + target) 
 
     self.history.append(nco_command)
     run_command(nco_command, self, silent) 
@@ -30,20 +41,19 @@ def ensemble_nco(self, method, silent = True):
     # clean up the directory
     cleanup(keep = self.current)
 
-#    return self
     
 
-def ensemble_min(self, silent = True):
+def ensemble_min(self, vars = None, silent = True):
     """Method to calculate an ensemble min from a list of files"""
-    return ensemble_nco(self, "min")
+    return ensemble_nco(self, "min", vars = vars)
 
-def ensemble_max(self, silent = True):
+def ensemble_max(self, vars = None, silent = True):
     """Method to calculate an ensemble max from a list of files"""
-    return ensemble_nco(self, "max")
+    return ensemble_nco(self, "max", vars = vars)
 
-def ensemble_mean(self, silent = True):
+def ensemble_mean(self, vars = None, silent = True):
     """Method to calculate an ensemble mean from a list of files"""
-    return ensemble_nco(self, "mean")
+    return ensemble_nco(self, "mean", vars = vars)
 
 
 
