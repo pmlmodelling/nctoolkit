b2c72e69bc70d5fadba68899a900965cd9267eaf
Robert Wilson <robertwilson190@gmail.com>
2019-09-13 09:05:48 +0100

    remove couple of lines that are now doing nothing

--- nchack/_cdo_command.py
+++ nchack/_cdo_command.py
@@ -6,15 +6,8 @@ from ._runthis import run_this
 def cdo_command(self, command, silent = True, cores = 1):
     """Method to call any cdo command of the the form 'command + infile + outfile'"""
 
-    if type(self.current) == list:
-        infile = str_flatten(self.current, " ")
-    else:
-        infile = self.current
-
     cdo_command = "cdo " + command
 
     run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
 
     cleanup(keep = self.current)
-
-    #return self
