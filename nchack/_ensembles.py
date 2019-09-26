9688b73590198ceb643e19e9b836f6379a107370
Robert Wilson <robertwilson190@gmail.com>
2019-09-26 16:52:11 +0100

    move ensemble checker to checkers

--- nchack/_ensembles.py
+++ nchack/_ensembles.py
@@ -94,35 +94,3 @@ def ensemble_range(self, silent = True):
     # clean up the directory
     cleanup(keep = self.current)
 
-
-
-def ensemble_check(self):
-    "A function to check an ensemble is valid"
-
-    results = []
-    for ff in self.current:
-        cdo_result = os.popen( "cdo partab " + ff).read()
-        results.append(cdo_result)
-
-    if len(list(set(results))) == 1:
-        parameters = True
-    else:
-        parameters = False 
-    if parameters == False:
-        print("the same parameters are not available in all files")
-
-    results = []
-
-    for ff in self.current:
-        cdo_result = os.popen( "cdo griddes " + ff).read()
-        results.append(cdo_result)
-
-    if len(list(set(results))) == 1:
-        grid = True
-    else:
-        grid = False 
-
-    if grid == False:
-        print("the same grid is not available in all files")
-
-
