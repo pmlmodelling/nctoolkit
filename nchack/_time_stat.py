80293f92586c79fc92e18770493e15d2e73d58d3
Robert Wilson <robertwilson190@gmail.com>
2019-10-08 14:03:27 +0100

    switch to using temp_file

--- nchack/_time_stat.py
+++ nchack/_time_stat.py
@@ -1,7 +1,7 @@
 from ._cleanup import cleanup
 from ._runthis import run_this
 from ._filetracker import nc_created
-import tempfile
+from ._temp_file import temp_file
 import os
 
 def time_stat(self, stat = "mean", silent = True, cores = 1):
@@ -39,33 +39,6 @@ def cum_sum(self, silent = True, cores = 1):
     
 def sum(self, silent = True, cores = 1):
     return time_stat(self, stat = "sum", silent = silent, cores = cores)
-
-
-
-#def percentile(self, p = 50, silent = True, cores = 1):
-#    """Method to calculate the percentile over all time steps"""
-#    if self.run == False:
-#        raise ValueError("You cannot currently run percentile in hold mode")
-#    minfile = tempfile.NamedTemporaryFile().name + ".nc"
-#    nc_created.append(minfile)
-#
-#    os.system("cdo timmin " + self.current + " " + minfile)
-#    if os.path.exists(minfile) == False:
-#        raise ValueError("Calculating a time minimum was not successful. Check output")
-#
-#    maxfile = tempfile.NamedTemporaryFile().name + ".nc"
-#    nc_created.append(maxfile)
-#
-#    os.system("cdo timmax " + self.current + " " + maxfile)
-#
-#    if os.path.exists(maxfile) == False:
-#        raise ValueError("Calculating a time maximum was not successful. Check output")
-#
-#    cdo_command = "cdo timpctl," + str(p) + " " + minfile + " " + maxfile  
-#    run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
-    # clean up the directory
-    cleanup(keep = self.current)
- 
 
 
 
