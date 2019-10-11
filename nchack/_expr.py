b36e86e693e48009117f8126b70ea6879e59bb49
Robert Wilson <robertwilson190@gmail.com>
2019-10-11 10:14:53 +0100

    add the ability to handle locals for expressions

--- nchack/_expr.py
+++ nchack/_expr.py
@@ -1,6 +1,25 @@
 
 from ._cleanup import cleanup
 from ._runthis import run_this
+import sys
+
+
+def fix_expr(expr):
+
+    expr = ''.join((' {} '.format(el) if el in '+-/*^()' else el for el in expr))
+    expr_split = expr.split(" ")
+    new_expr = ""
+
+    for x in expr_split:
+        if x.startswith("@"):
+            # We need to first check the local variable supplied is a numeric
+            if (isinstance(eval("sys.modules['__main__']." + x.replace("@", "")), (int, float))) == False:
+                raise ValueError(x +  " is not numeric!")
+            
+            new_expr +=  str(eval("sys.modules['__main__']." + x.replace("@", "")))
+        else:
+            new_expr +=  x
+    return new_expr
 
 def expression(self, operations = None, method = "expr", silent = True, cores = 1):
     """Method to modify a netcdf file using expr"""
@@ -13,11 +32,12 @@ def expression(self, operations = None, method = "expr", silent = True, cores = 
     expr = []
 
     for key,value in operations.items():
-        expr.append(key + "=" + value)
+        expr.append(key + "=" + fix_expr(value))
         
     expr = ";".join(expr)
     expr = expr.replace(" ", "" )
     expr = '"' + expr + '"'
+
 
     cdo_command = "cdo -" + method + "," + expr
     run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
