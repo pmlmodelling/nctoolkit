1488561764eeffe4cbd83b8c30860e565d8a107d
Robert Wilson <robertwilson190@gmail.com>
2019-10-17 16:48:54 +0100

    add join option

--- nchack/_cellareas.py
+++ nchack/_cellareas.py
@@ -1,7 +1,11 @@
 from ._cleanup import cleanup
 from ._runthis import run_this
+from ._runthis import run_cdo
+from ._temp_file import temp_file
+from ._filetracker import nc_created
 
-def cell_areas(self, silent = True, cores = 1):
+
+def cell_areas(self, silent = True, cores = 1, join = True):
     """
     Calculate the cell areas in square meters
 
@@ -17,6 +21,25 @@ def cell_areas(self, silent = True, cores = 1):
 
     """
 
-    cdo_command = "cdo -gridarea "
-    run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
+    if join and self.run == False:
+        self.release()
+
+    if join:
+        target = temp_file(".nc")
+        nc_created.append(target)
+        cdo_command = "cdo -gridarea " + self.current + " " + target
+        self.history.append(cdo_command)
+        run_cdo(cdo_command, target)
+        
+        new_target = temp_file(".nc")
+        nc_created.append(new_target)
+        cdo_command = "cdo -L -merge " + self.current + " " + target + " " + new_target
+        self.history.append(cdo_command)
+        run_cdo(cdo_command, new_target)
+        self.current = new_target
+    else:
+        run_this(cdo_command, self, silent, output = "ensemble", cores = cores)
+
+
+
     cleanup(keep = self.current)
