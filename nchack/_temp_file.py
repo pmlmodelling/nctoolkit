0d878595dbb2451559b647f7d593280955cf47f9
Robert Wilson <robertwilson190@gmail.com>
2019-10-23 15:23:05 +0100

    add automatic tempdir switch when disk space is running out

--- nchack/_temp_file.py
+++ nchack/_temp_file.py
@@ -6,6 +6,15 @@ from ._session import session_stamp
 
 def temp_file(ext = ""):
 
+        # check space left in temp dir and switch it if there isn't much
+        if session_stamp["temp_dir"] == "/tmp/":
+            result = os.statvfs("/tmp/")
+            result = result.f_frsize * result.f_bavail
+
+            if result <  0.5 * 1e9:
+                if session_stamp["temp_dir"] == "/tmp/":
+                    session_stamp["temp_dir"] = "/var/tmp/"
+
             actual_temp = session_stamp["temp_dir"]
             actual_temp = actual_temp + "/"
             actual_temp = actual_temp.replace("//", "/")
