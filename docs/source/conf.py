c4b0eabd4be623f6632070850934f1eca16f92e8
Robert Wilson <robertwilson190@gmail.com>
2019-10-24 16:26:03 +0100

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-# import os
-# import sys
+import os
+import sys
+import nchack
+sys.path.insert(0, os.path.abspath('../..'))
 # sys.path.insert(0, os.path.abspath('.'))
 
 
