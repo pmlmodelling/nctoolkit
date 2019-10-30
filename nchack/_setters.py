afc73e40adfb21b56a42fc239bf0fc3d2ffaeaab
Robert Wilson <robertwilson190@gmail.com>
2019-10-30 16:06:08 +0000

    import nc_safe

--- nchack/_setters.py
+++ nchack/_setters.py
@@ -5,6 +5,7 @@ import os
 
 from ._temp_file import temp_file
 from ._filetracker import nc_created
+from ._filetracker import nc_safe
 from ._cleanup import cleanup
 from ._runthis import run_this
 
