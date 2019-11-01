5f2f25ea2c2b3bb60f863f1b6512484d71a9fb04
Robert Wilson <robertwilson190@gmail.com>
2019-11-01 16:30:34 +0000

    add test1.py

---
+++ test1.py
@@ -0,0 +1,42 @@
+import unittest
+import nchack as nc
+import pandas as pd
+import xarray as xr
+import os
+
+
+class TestSelect(unittest.TestCase):
+
+    def test_clim1(self):
+        ff = "data/sst.mon.mean.nc"
+        tracker = nc.open_data(ff)
+        tracker.split_year()
+        tracker.ensemble_mean()
+        x = tracker.to_xarray().sst.values[0][0][0].astype("float")
+        tracker = nc.open_data(ff)
+        tracker.monthly_mean_climatology()
+        y = tracker.to_xarray().sst.values[0][0][0].astype("float")
+        self.assertEqual(x, -1.7077397108078003) 
+        self.assertEqual(x, y)
+
+
+    def test_clim2(self):
+        ff = "data/sst.mon.mean.nc"
+        tracker = nc.open_data(ff)
+        tracker.split_year()
+        tracker.clip(lon = [50, 60])
+        tracker.ensemble_mean()
+        tracker.spatial_mean()
+        x = tracker.to_xarray().sst.values[0][0][0].astype("float")
+        tracker = nc.open_data(ff)
+        tracker.monthly_mean_climatology()
+        tracker.clip(lon = [50, 60])
+        tracker.spatial_mean()
+        y = tracker.to_xarray().sst.values[0][0][0].astype("float")
+        self.assertEqual(x, 19.814777374267578) 
+        self.assertEqual(x, y)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
