9254273535fb75487fd4ed5a3818c97a9af3701c
Robert Wilson <robertwilson190@gmail.com>
2019-11-07 08:46:21 +0000

    tidy up docstring

--- nchack/_remove.py
+++ nchack/_remove.py
@@ -2,8 +2,12 @@
 import os
 
 def nc_remove(ff):
-    """Method for removing netcdf files. This is ultra-safe and makes sure the file is in the tmp directory before deleting""" 
+    """
+    Method for removing netcdf files. 
+    This is ultra-safe and makes sure the file is in the tmp directory before deleting
+    """ 
     if (ff.startswith("/tmp") or ff.startswith("/var/tmp/") or ff.startswith("/usr/tmp/")) == False:
         raise ValueError("The file " + ff + " is not in a tmp folder")
+
     os.remove(ff)
         
