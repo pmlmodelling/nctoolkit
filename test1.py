ef714c7e898e45a288638ac7b1a94d4101bf4688
Robert Wilson <robertwilson190@gmail.com>
2019-11-08 10:28:49 +0000

    add test for cdo_command

--- test1.py
+++ test1.py
@@ -36,6 +36,18 @@ class TestSelect(unittest.TestCase):
         self.assertEqual(x, 19.814777374267578) 
         self.assertEqual(x, y)
 
+    def test_cdocommand(self):
+        ff = "data/sst.mon.mean.nc"
+        tracker = nc.open_data(ff)
+        tracker.select_months(1)
+        tracker.spatial_mean()
+        x = tracker.to_xarray().sst.values[0][0][0].astype("float")
+        tracker = nc.open_data(ff)
+        tracker.cdo_command("cdo selmon,1")
+        tracker.spatial_mean()
+        y = tracker.to_xarray().sst.values[0][0][0].astype("float")
+
+        self.assertEqual(x, y)
 
 if __name__ == '__main__':
     unittest.main()
