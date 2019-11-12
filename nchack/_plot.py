fc8cb957e5e5db194f1bdc7141ddb8d83ad2eb55
Robert Wilson <robertwilson190@gmail.com>
2019-11-12 14:37:39 +0000

    set decode_times to False when needed

--- nchack/_plot.py
+++ nchack/_plot.py
@@ -37,6 +37,13 @@ def autoplot(self, log = False, panel = False):
     cdo_result = subprocess.run("cdo ngridpoints " + self.current, shell = True, capture_output = True)
     n_points = int(str(cdo_result.stdout).replace("b'", "").split("\\n")[0])
 
+    decode_time = False
+    if n_times > 1:
+        try:
+            x = xr.open_dataset(self.current)
+            decode_times = True
+        except:
+            decode_times = False
 
     # Case when there is only a single map to show
 
@@ -53,14 +60,14 @@ def autoplot(self, log = False, panel = False):
         out = subprocess.run("cdo griddes " + self.current, shell = True, capture_output = True)
         lon_name = [x for x in str(out.stdout).replace("b'", "").split("\\n") if "xname" in x][0].split(" ")[-1]
         lat_name = [x for x in str(out.stdout).replace("b'", "").split("\\n") if "yname" in x][0].split(" ")[-1]
-        data = self.to_xarray()
+        data = self.to_xarray(decode_times = decode_times)
         data = data.squeeze([lon_name, lat_name])
         data = data.rename({self.variables[0]: "x"})
         return data.x.hvplot()
 
     if n_times > 1 and n_points < 2 and n_levels <= 1 and len(self.variables) > 1:
 
-        df = self.to_xarray()
+        df = self.to_xarray(decode_times = decode_times)
 
         dim_dict = dict(df.dims)
         to_go = []
@@ -83,13 +90,13 @@ def autoplot(self, log = False, panel = False):
         lat_name = [x for x in str(out.stdout).replace("b'", "").split("\\n") if "yname" in x][0].split(" ")[-1]
 
         variables = self.variables
-        self_max = self.to_xarray().rename({self.variables[0]: "x"}).x.max()
-        self_min = self.to_xarray().rename({self.variables[0]: "x"}).x.min()
+        self_max = self.to_xarray(decode_times = decode_times).rename({self.variables[0]: "x"}).x.max()
+        self_min = self.to_xarray(decode_times = decode_times).rename({self.variables[0]: "x"}).x.min()
         v_max = max(self_max.values, -self_min.values)
         if self_max.values > 0 and self_min.values < 0:
-            return self.to_xarray().hvplot.image(lon_name, lat_name, self.variables[0], dynamic = False,  logz = log, cmap = "seismic").redim.range(**{self.variables[0]:(-v_max,v_max)})
+            return self.to_xarray(decode_times = decode_times).hvplot.image(lon_name, lat_name, self.variables[0], dynamic = False,  logz = log, cmap = "seismic").redim.range(**{self.variables[0]:(-v_max,v_max)})
         else:
-            return self.to_xarray().hvplot.image(lon_name, lat_name, self.variables[0], dynamic = False,  logz = log, cmap = "viridis").redim.range(**{self.variables[0]:(-self_min.values, v_max)})
+            return self.to_xarray(decode_times = decode_times).hvplot.image(lon_name, lat_name, self.variables[0], dynamic = False,  logz = log, cmap = "viridis").redim.range(**{self.variables[0]:(-self_min.values, v_max)})
 
     if n_points > 1 and n_levels <= 1:
         out = subprocess.run("cdo griddes " + self.current, shell = True, capture_output = True)
@@ -97,7 +104,7 @@ def autoplot(self, log = False, panel = False):
         lat_name = [x for x in str(out.stdout).replace("b'", "").split("\\n") if "yname" in x][0].split(" ")[-1]
 
         variables = self.variables
-        return self.to_xarray().hvplot.image(lon_name, lat_name, variables, dynamic = False, cmap = "viridis", logz = log)
+        return self.to_xarray(decode_times = decode_times).hvplot.image(lon_name, lat_name, variables, dynamic = False, cmap = "viridis", logz = log)
 
 
 
