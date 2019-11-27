5a597be42d637d5fb2e70e9b907e7b48f10db06e
Robert Wilson <robertwilson190@gmail.com>
2019-11-27 15:23:52 +0000

    ensure everything has been evaluated

--- nchack/toxarray.py
+++ nchack/toxarray.py
@@ -7,14 +7,15 @@ def to_xarray(self, decode_times = True):
     Parameters
     -------------
     decode_times: boolean
-        Set to False if you do not want xarray to decode the times. Default is True. 
+        Set to False if you do not want xarray to decode the times. Default is True.
 
     """
 
-    
+    self.release()
+
     if type(self.current) is str:
         data = xr.open_dataset(self.current, decode_times = decode_times)
-    else:   
+    else:
         data = xr.open_mfdataset(self.current, decode_times = decode_times)
 
     return data
