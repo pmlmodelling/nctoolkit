2f72732f086539e9b7fa7bb017fee2a734705e45
Robert Wilson <robertwilson190@gmail.com>
2019-11-28 15:11:11 +0000

    switch to using inspect to find local variables

--- nchack/expr.py
+++ nchack/expr.py
@@ -1,45 +1,8 @@
 
 from .runthis import run_this
 import sys
+import inspect
 
-
-def fix_expr(expr):
-    """Function to fix expressions that use locals"""
-
-    expr = ''.join((' {} '.format(el) if el in '=><+-/*^()' else el for el in expr))
-    expr_split = expr.split(" ")
-    new_expr = ""
-
-    for x in expr_split:
-        if x.startswith("@"):
-            # We need to first check the local variable supplied is a numeric
-            if (isinstance(eval("sys.modules['__main__']." + x.replace("@", "")), (int, float))) == False:
-                raise TypeError(x +  " is not numeric!")
-            new_expr +=  str(eval("sys.modules['__main__']." + x.replace("@", "")))
-        else:
-            new_expr +=  x
-    return new_expr
-
-def expression(self, operations = None, method = "expr"):
-    """Method to modify a netcdf file using expr"""
-
-    if type(operations) is not dict:
-        raise TypeError("No expression was provided")
-
-    # first,we need to convert the operations dictionary to a cdo expression
-
-    expr = []
-
-    for key,value in operations.items():
-        expr.append(key + "=" + fix_expr(value))
-
-    expr = ";".join(expr)
-    expr = expr.replace(" ", "" )
-    expr = '"' + expr + '"'
-
-
-    cdo_command = "cdo -" + method + "," + expr
-    run_this(cdo_command, self, output = "ensemble")
 
 
 def transmute(self, operations = None):
@@ -52,8 +15,42 @@ def transmute(self, operations = None):
         operations to apply. The keys are the new variables to generate. The values are the mathematical operations to carry out.
 
     """
-    return expression(self, operations = operations, method = "expr")
 
+    method = "expr"
+
+    if type(operations) is not dict:
+        raise TypeError("No expression was provided")
+
+    # first,we need to convert the operations dictionary to a cdo expression
+
+    expr = []
+
+    for key,value in operations.items():
+
+        new_x = ''.join((' {} '.format(el) if el in '=><+-/*^()' else el for el in value))
+        expr_split = new_x.split(" ")
+        new_expr = ""
+
+        for x in expr_split:
+            if x.startswith("@"):
+                # We need to first check the local variable supplied is a numeric
+                new_x = inspect.currentframe().f_back.f_locals[x.replace("@", "")]
+                if isinstance(new_x, (int, float)) == False:
+                    raise TypeError(x +  " is not numeric!")
+                new_expr +=  str(new_x)
+            else:
+                new_expr +=  x
+
+        expr.append(key + "=" + new_expr)
+
+
+    expr = ";".join(expr)
+    expr = expr.replace(" ", "" )
+    expr = '"' + expr + '"'
+
+
+    cdo_command = "cdo -" + method + "," + expr
+    run_this(cdo_command, self, output = "ensemble")
 
 def mutate(self, operations = None):
     """
@@ -65,7 +62,43 @@ def mutate(self, operations = None):
         operations to apply. The keys are the new variables to generate. The values are the mathematical operations to carry out.
 
     """
-    return expression(self, operations = operations, method = "aexpr")
+
+    method = "aexpr"
+
+    if type(operations) is not dict:
+        raise TypeError("No expression was provided")
+
+    # first,we need to convert the operations dictionary to a cdo expression
+
+    expr = []
+
+    for key,value in operations.items():
+
+        new_x = ''.join((' {} '.format(el) if el in '=><+-/*^()' else el for el in value))
+        expr_split = new_x.split(" ")
+        new_expr = ""
+
+        for x in expr_split:
+            if x.startswith("@"):
+                # We need to first check the local variable supplied is a numeric
+                new_x = inspect.currentframe().f_back.f_locals[x.replace("@", "")]
+                if isinstance(new_x, (int, float)) == False:
+                    raise TypeError(x +  " is not numeric!")
+                new_expr +=  str(new_x)
+            else:
+                new_expr +=  x
+
+        expr.append(key + "=" + new_expr)
+
+
+    expr = ";".join(expr)
+    expr = expr.replace(" ", "" )
+    expr = '"' + expr + '"'
+
+
+    cdo_command = "cdo -" + method + "," + expr
+    run_this(cdo_command, self, output = "ensemble")
+
 
 
 
