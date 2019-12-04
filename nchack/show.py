19a091d2a12b7b5155583d5a660ba855c725ed5a
Robert Wilson <robertwilson190@gmail.com>
2019-12-04 08:47:46 +0000

    fix file name bug in nc_variables

--- nchack/show.py
+++ nchack/show.py
@@ -31,7 +31,7 @@ def levels(self):
 
 
 def nc_variables(ff):
-    cdo_result = subprocess.run("cdo showname" + self.current, shell = True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    cdo_result = subprocess.run("cdo showname" + ff, shell = True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     cdo_result = str(cdo_result.stdout).replace("\\n", "")
     cdo_result = cdo_result.replace("b'", "").strip()
     cdo_result = cdo_result.replace("'", "").strip()
