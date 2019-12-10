70ab8d9c82c93a3def5e60e69609ec4c562bb17d
Robert Wilson <robertwilson190@gmail.com>
2019-12-10 16:44:11 +0000

    decode times using cdo when xarray cannot

--- nchack/toxarray.py
+++ nchack/toxarray.py
@@ -1,4 +1,7 @@
 import xarray as xr
+from datetime import datetime
+
+
 
 def to_xarray(self, decode_times = True):
     """
@@ -13,9 +16,42 @@ def to_xarray(self, decode_times = True):
 
     self.release()
 
+    if decode_times is False:
+        if type(self.current) is str:
+            data = xr.open_dataset(self.current, decode_times = decode_times)
+        else:
+            data = xr.open_mfdataset(self.current, decode_times = decode_times)
+        return data
+
+    cdo_times = True
+    try:
+        x = xr.open_dataset(self.current)
+        cdo_times = False
+    except:
+        cdo_times = True
+
+    if cdo_times is False:
+        if type(self.current) is str:
+            data = xr.open_dataset(self.current, decode_times = decode_times)
+        else:
+            data = xr.open_mfdataset(self.current, decode_times = decode_times)
+        return data
+
+    # get the times
+
     if type(self.current) is str:
-        data = xr.open_dataset(self.current, decode_times = decode_times)
+
+        times = [datetime.strptime(ss.replace("T", " "), '%Y-%m-%d %H:%M:%S') for ss in self.times()]
+
+        data = xr.open_dataset(self.current, decode_times = False)
+        data = data.assign_coords(time = times)
+        return data
+
     else:
         data = xr.open_mfdataset(self.current, decode_times = decode_times)
+        return data
 
-    return data
+
+
+
+
