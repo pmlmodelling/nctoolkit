153b2c7d9d20b123e71a093de51d097272fd63b4
Robert Wilson <robertwilson190@gmail.com>
2019-12-10 16:44:58 +0000

    let to_xarray decode times

--- nchack/plot.py
+++ nchack/plot.py
@@ -62,14 +62,14 @@ def plot(self, log = False, panel = False):
         out = subprocess.run("cdo griddes " + self.current, shell = True, stdout=subprocess.PIPE, stderr =subprocess.PIPE)
         lon_name = [x for x in str(out.stdout).replace("b'", "").split("\\n") if "xname" in x][0].split(" ")[-1]
         lat_name = [x for x in str(out.stdout).replace("b'", "").split("\\n") if "yname" in x][0].split(" ")[-1]
-        data = self.to_xarray(decode_times = decode_times)
+        data = self.to_xarray()
         data = data.squeeze([lon_name, lat_name])
         data = data.rename({self.variables[0]: "x"})
         return data.x.hvplot()
 
     if n_times > 1 and n_points < 2 and n_levels <= 1 and len(self.variables) > 1:
 
-        df = self.to_xarray(decode_times = decode_times)
+        df = self.to_xarray()
 
         dim_dict = dict(df.dims)
         to_go = []
@@ -92,13 +92,13 @@ def plot(self, log = False, panel = False):
         lat_name = [x for x in str(out.stdout).replace("b'", "").split("\\n") if "yname" in x][0].split(" ")[-1]
 
         variables = self.variables
-        self_max = self.to_xarray(decode_times = decode_times).rename({self.variables[0]: "x"}).x.max()
-        self_min = self.to_xarray(decode_times = decode_times).rename({self.variables[0]: "x"}).x.min()
+        self_max = self.to_xarray().rename({self.variables[0]: "x"}).x.max()
+        self_min = self.to_xarray().rename({self.variables[0]: "x"}).x.min()
         v_max = max(self_max.values, -self_min.values)
         if self_max.values > 0 and self_min.values < 0:
-            return self.to_xarray(decode_times = decode_times).hvplot.image(lon_name, lat_name, self.variables[0], dynamic = True,  logz = log, cmap = "seismic").redim.range(**{self.variables[0]:(-v_max,v_max)})
+            return self.to_xarray().hvplot.image(lon_name, lat_name, self.variables[0], dynamic = True,  logz = log, cmap = "seismic").redim.range(**{self.variables[0]:(-v_max,v_max)})
         else:
-            return self.to_xarray(decode_times = decode_times).hvplot.image(lon_name, lat_name, self.variables[0], dynamic = True,  logz = log, cmap = "viridis").redim.range(**{self.variables[0]:(-self_min.values, v_max)})
+            return self.to_xarray().hvplot.image(lon_name, lat_name, self.variables[0], dynamic = True,  logz = log, cmap = "viridis").redim.range(**{self.variables[0]:(-self_min.values, v_max)})
 
     if n_points > 1 and n_levels <= 1 and len(self.variables) > 1:
         out = subprocess.run("cdo griddes " + self.current, shell = True, stdout=subprocess.PIPE, stderr =subprocess.PIPE)
@@ -106,7 +106,7 @@ def plot(self, log = False, panel = False):
         lat_name = [x for x in str(out.stdout).replace("b'", "").split("\\n") if "yname" in x][0].split(" ")[-1]
 
         variables = self.variables
-        return self.to_xarray(decode_times = decode_times).hvplot.image(lon_name, lat_name, variables, dynamic = True, cmap = "viridis", logz = log)
+        return self.to_xarray().hvplot.image(lon_name, lat_name, variables, dynamic = True, cmap = "viridis", logz = log)
 
 
 
