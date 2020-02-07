7cdca103f45ddb43af2a7091a3ffd885e62db834
Robert Wilson <robertwilson190@gmail.com>
2020-02-07 16:32:04 +0000

    improve error checks

--- nchack/masking.py
+++ nchack/masking.py
@@ -13,20 +13,35 @@ def mask_box(self, lon = [-180, 180], lat = [-90, 90]):
         Latitude range to mask. Must be of the form: [lat_min, lat_max]
     """
 
-    if (type(lon) is not list) or (type(lat) is not list):
-        raise TypeError("Check that lon/lat ranges are tuples")
 
-    if(type(lon[0]) is float ) or ( type(lon[0]) is int) == False:
+    if  type(lon) is not list or type(lat) is not list:
+         raise TypeError("Check that lon/lat ranges are tuples")
+
+    if len(lon) > 2:
+        raise ValueError("lon is a list of more than 2 variables")
+
+    if len(lat) > 2:
+        raise ValueError("lat is a list of more than 2 variables")
+
+    if ( type(lon[0]) is float  or  type(lon[0]) is int ) == False:
         raise TypeError("Check lon")
 
-    if( type(lon[1]) is float ) or ( type(lon[1]) is int) == False:
+    if ( type(lon[1]) is float  or  type(lon[1]) is int ) == False:
         raise TypeError("Check lon")
 
-    if( type(lat[0]) is float ) or ( type(lat[0]) is int) == False:
+    if ( type(lat[0]) is float  or  type(lat[0]) is int ) == False:
         raise TypeError("Check lat")
 
-    if( type(lat[1]) is float ) or ( type(lat[1]) is int) == False:
+    if ( type(lat[1]) is float  or  type(lat[1]) is int ) == False:
         raise TypeError("Check lat")
+
+    # now, clip to the lonlat box we need
+
+    if  lat[1] < lat[0]:
+        raise ValueError("Check lat order")
+    if  lon[1] < lon[0]:
+        raise ValueError("Check lon order")
+
 
     # now, clip to the lonlat box we need
 
