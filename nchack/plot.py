5f46aefd9adb9b87aad62c0f160a43ed648889cc
Robert Wilson <robertwilson190@gmail.com>
2020-02-12 10:11:48 +0000

    make 1 var case work for varying depth levels

--- nchack/plot.py
+++ nchack/plot.py
@@ -71,7 +71,7 @@ def plot(self, log = False, panel = False):
         else:
             return df.reset_index().set_index("time").loc[:, self.variables].reset_index().melt("time").set_index("time").hvplot(groupby = "variable", logy = log, dynamic = True)
 
-    if n_points > 1 and n_levels <= 1 and len(self.variables) == 1:
+    if n_points > 1 and len(self.variables) == 1:
         out = subprocess.run("cdo griddes " + self.current, shell = True, stdout=subprocess.PIPE, stderr = subprocess.PIPE)
         lon_name = [x for x in str(out.stdout).replace("b'", "").split("\\n") if "xname" in x][-1].split(" ")[-1]
         lat_name = [x for x in str(out.stdout).replace("b'", "").split("\\n") if "yname" in x][-1].split(" ")[-1]
@@ -85,7 +85,7 @@ def plot(self, log = False, panel = False):
         else:
             return self.to_xarray().hvplot.image(lon_name, lat_name, self.variables[0], dynamic = True,  logz = log, cmap = "viridis").redim.range(**{self.variables[0]:(-self_min.values, v_max)})
 
-    if n_points > 1 and n_levels <= 1 and len(self.variables) > 1:
+    if n_points > 1 and n_levels <=1  and len(self.variables) > 1:
         out = subprocess.run("cdo griddes " + self.current, shell = True, stdout=subprocess.PIPE, stderr =subprocess.PIPE)
         lon_name = [x for x in str(out.stdout).replace("b'", "").split("\\n") if "xname" in x][-1].split(" ")[-1]
         lat_name = [x for x in str(out.stdout).replace("b'", "").split("\\n") if "yname" in x][-1].split(" ")[-1]
