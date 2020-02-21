2eed691da934c140bbb1c2f38daa2d89bcb8bf40
Robert Wilson <robertwilson190@gmail.com>
2020-02-21 08:54:07 +0000

    show message saying checks occuring in open_data

--- nchack/api.py
+++ nchack/api.py
@@ -156,11 +156,11 @@ def open_data(x = None):
             warnings.warn(message = "Duplicates in data set have been removed!")
 
     if type(x) is list:
+        print("Performing basic checks on ensemble files")
         if len(x) == 0:
             raise ValueError("You have not provided any files!")
+
         for ff in x:
-
-
 
             if os.path.exists(ff) == False:
                 raise ValueError("Data set " + ff + " does not exist!")
@@ -172,6 +172,7 @@ def open_data(x = None):
                     raise ValueError(mes)
                 nc_safe.append(ff)
                 nc_protected.append(x)
+        print("Files pass checks")
 
     # if there is only one file in the list, change it to a single file
     if type(x) is list:
