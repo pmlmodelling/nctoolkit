ff8b711028ca3d192243b5dafb7e466c0c7e33ae
Robert Wilson <robertwilson190@gmail.com>
2020-02-24 13:53:33 +0000

    add ability to select variable to use in operation

--- nchack/add_etc.py
+++ nchack/add_etc.py
@@ -17,8 +17,16 @@ def arithall(self, stat = "divc", x = None):
     run_this(cdo_command, self,  output = "ensemble")
 
 
-def operation(self, method = "mul", ff = None):
+def operation(self, method = "mul", ff = None, var = None):
     """Method to add, subtract etc. a netcdf file from another one"""
+
+
+    if var is not None:
+        if type(var) is not str:
+            raise TypeError("var supplied is not a string")
+        if var not in nc_variables(ff):
+            raise ValueError("var supplied is not available in the dataset")
+
 
     self.release()
 
@@ -27,14 +35,18 @@ def operation(self, method = "mul", ff = None):
     for x in self:
 
     # check that the datasets can actually be worked with
-        if len(nc_variables(ff)) != len(nc_variables(x)) and len(nc_variables(ff)) != 1:
-            raise ValueError("Datasets have incompatible variable numbers for the operation!")
+        if var is None:
+            if len(nc_variables(ff)) != len(nc_variables(x)) and len(nc_variables(ff)) != 1:
+                raise ValueError("Datasets have incompatible variable numbers for the operation!")
 
         # create the temp file
         target = temp_file(".nc")
 
         # create the system call
-        cdo_command = "cdo -L " + method + " "  + x + " " + ff + " " + target
+        if var is None:
+            cdo_command = "cdo -L " + method + " "  + x + " " + ff + " " + target
+        else:
+            cdo_command = "cdo -L " + method + " "  + x + " -selname," + var + " " + ff + " " + target
 
         # modify system call if threadsafe
         if session_info["thread_safe"]:
@@ -65,13 +77,15 @@ def operation(self, method = "mul", ff = None):
 
 
 
-def multiply(self, x = None):
+def multiply(self, x = None, var = None):
     """
     Multiply a dataset
     Parameters
     ------------
     x: int, float, DataSet or netcdf file
         An int, float, single file dataset or netcdf file to multiply the dataset by
+    var: str
+        A variable in the x to multiply the dataset by
     """
 
     # 1: int, float multiplication
@@ -89,16 +103,18 @@ def multiply(self, x = None):
     if type(ff) is not str:
         raise TypeError("You have not provided an int, float, dataset or file path!")
 
-    operation(self = self, method = "mul", ff = ff)
+    operation(self = self, method = "mul", ff = ff, var = var)
 
 
-def subtract(self, x = None):
+def subtract(self, x = None, var = None):
     """
     Subtract from a dataset
     Parameters
     ------------
     x: int, float, DataSet or netcdf file
         An int, float, single file dataset or netcdf file to subtract from the dataset
+    var: str
+        A variable in the x to use for the operation
     """
 
     # 1: int, float subtraction
@@ -116,16 +132,18 @@ def subtract(self, x = None):
     if type(ff) is not str:
         raise TypeError("You have not provided an int, float, dataset or file path!")
 
-    operation(self = self, method = "sub", ff = ff)
+    operation(self = self, method = "sub", ff = ff, var = var)
 
 
-def add(self, x = None):
+def add(self, x = None, var = None):
     """
     Add to a dataset
     Parameters
     ------------
     x: int, float, DataSet or netcdf file
         An int, float, single file dataset or netcdf file to add to the dataset
+    var: str
+        A variable in the x to use for the operation
     """
 
     # 1: int, float addition
@@ -143,17 +161,19 @@ def add(self, x = None):
     if type(ff) is not str:
         raise TypeError("You have not provided an int, float, dataset or file path!")
 
-    operation(self = self, method = "add", ff = ff)
+    operation(self = self, method = "add", ff = ff, var = var)
 
 
 
-def divide(self, x = None):
+def divide(self, x = None, var = None):
     """
     Divide the data in the current dataset by the data in another dataset or netcdf file
     Parameters
     ------------
     x: int, float, DataSet or netcdf file
         An int, float, single file dataset or netcdf file to divide the dataset by
+    var: str
+        A variable in the x to use for the operation
     """
 
     # 1: int, float division
@@ -171,7 +191,7 @@ def divide(self, x = None):
     if type(ff) is not str:
         raise TypeError("You have not provided an int, float, dataset or file path!")
 
-    operation(self = self, method = "div", ff = ff)
+    operation(self = self, method = "div", ff = ff, var = var)
 
 
 
