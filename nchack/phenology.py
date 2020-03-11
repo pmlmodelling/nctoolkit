1c68d5621c02586425254fb8b314599e883d6a1d
Robert Wilson <robertwilson190@gmail.com>
2020-03-11 11:06:15 +0000

    simplify peak code

--- nchack/phenology.py
+++ nchack/phenology.py
@@ -44,83 +44,32 @@ def phenology(self, var = None, metric = "middle"):
         target = run_cdo(command, target = target)
         self.history.append(command)
         self._hold_history = copy.deepcopy(self.history)
+        if self.current in nc_safe:
+            nc_safe.remove(self.current)
         self.current = target
+
+        nc_safe.append(self.current)
 
 
         return None
 
+    if metric == "peak":
+        target = temp_file(".nc")
+        command = f"cdo -L -timmin -setrtomiss,-10000,0 -expr,'peak=var*ctimestep()' -eq -chname,{var},var -selname,{var} {self.current} -timmax -chname,{var},var -selname,{var} {self.current} {target}"
+
+        target = run_cdo(command, target = target)
+        self.history.append(command)
+        self._hold_history = copy.deepcopy(self.history)
+
+        if self.current in nc_safe:
+            nc_safe.remove(self.current)
+        self.current = target
+
+        nc_safe.append(self.current)
+
+        return None
 
 
-    #  create a new tracker for the phenologies
-    # Then restrict the file to the var selected
-
-
-    start_files = copy.deepcopy(self.current)
-
-    new_self = self.copy()
-
-    new_self.select_variables(var)
-
-    # Create the day of year
-
-    doy_nc = temp_file("nc")
-
-    #cdo_command = "cdo -L -timcumsum -chname," + var +   ",peak -setclonlatbox,1,-180,180,-90,90 -selname," + var + " " + new_self.current + " " + doy_nc
-    cdo_command = f"cdo -L -timcumsum -chname,{var},peak -setclonlatbox,1,-180,180,-90,90 -selname,{var} {new_self.current} {doy_nc}"
-
-    new_self.history.append(cdo_command)
-
-    doy_nc = run_cdo(cdo_command, doy_nc)
-
-    # Find the max value of the var
-
-    max_nc = temp_file("nc")
-
-    cdo_command = f"cdo -L -timmax -chname,{var},{var}_max -selname,{var} {new_self.current} {max_nc}"
-
-    new_self.history.append(cdo_command)
-    max_nc = run_cdo(cdo_command, max_nc)
-
-    # We now need to merge the three  netcdf files
-
-    out_nc = temp_file("nc")
-
-    cdo_command = f"cdo merge {new_self.current} {max_nc} {doy_nc} {out_nc}"
-
-    new_self.history.append(cdo_command)
-    out_nc = run_cdo(cdo_command, out_nc)
-
-    # Now, calculate the timing of the annual maximum
-
-    phen_nc = temp_file("nc")
-
-    cdo_command = f"cdo -L -timmin -selname,peak -expr,'peak=peak + 365*({var}<{var}_max)' {out_nc} {phen_nc}"
-
-    new_self.history.append(cdo_command)
-    phen_nc = run_cdo(cdo_command, phen_nc)
-
-    nc_safe.remove(new_self.current)
-
-    nc_safe.append(phen_nc)
-
-    new_self.current = phen_nc
-
-    # set the long name and unit
-
-
-    new_self._hold_history = copy.deepcopy(new_self.history)
-
-    new_self.set_longnames({"peak": "Timing of annual maximum"})
-
-    new_self.set_units({"peak": "Day of year"})
-    new_self.release()
-
-    self.current = copy.deepcopy(new_self.current )
-    self.history+=copy.deepcopy(new_self.history)
-    self._hold_history = copy.deepcopy(self.history)
-    nc_safe.append(self.current)
-
-    nc_safe.remove(start_files)
 
     cleanup(self.current)
 
