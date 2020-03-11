35a44a7b7ecd7a3657cdfb31e373b41a23200516
Robert Wilson <robertwilson190@gmail.com>
2020-03-11 11:07:59 +0000

    add test for reduce_dim

---
+++ tests/test_reduce.py
@@ -0,0 +1,28 @@
+import unittest
+import nchack as nc
+import pandas as pd
+import xarray as xr
+import os
+
+
+
+ff = "data/sst.mon.mean.nc"
+
+class TestSelect(unittest.TestCase):
+
+    def test_reduce(self):
+        nc.options(lazy = False)
+        tracker = nc.open_data(ff)
+        tracker.mean()
+        tracker.reduce_dims()
+        n = len(tracker.times())
+        print(nc.session.session_info)
+        self.assertEqual(n, 0)
+        nc.options(lazy = True)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
