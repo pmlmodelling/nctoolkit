0ab574b5c36ce5dd55b67226cd7805f448fad04d
Robert Wilson <robertwilson190@gmail.com>
2020-03-16 10:58:27 +0000

    make phenology work with ensembles

--- nchack/phenology.py
+++ nchack/phenology.py
@@ -8,6 +8,7 @@ from .setters import set_longnames
 from .cleanup import cleanup
 from .cleanup import disk_clean
 from .runthis import run_cdo
+from .show import nc_years
 import copy
 
 
@@ -36,28 +37,23 @@ def phenology(self, var = None, metric = None, p = None):
 
     self.release()
 
-    # First step is to check if the current file exists
-    if type(self.current) is not str:
-        raise TypeError("This method only works on single files")
 
-    if len(self.years()) > 1:
-        raise ValueError("This can only work with single year data currently")
-
-    if metric == "middle":
+    if metric == "peak":
 
         new_files = []
         new_commands = []
 
         for ff in self:
+            if len(nc_years(ff)) > 1:
+                raise ValueError("This can only work with single year data currently")
 
             target = temp_file(".nc")
-            command = f"cdo -L -timmin -setrtomiss,-10000,0 -expr,'middle=var*ctimestep()' -gt -timcumsum -chname,{var},var -selname,{var} {ff} -divc,2 -timsum -chname,{var},var -selname,{var} {self.current} {target}"
+            command = f"cdo -L -timmin -setrtomiss,-10000,0 -expr,'peak=var*ctimestep()' -eq -chname,{var},var -selname,{var} {ff} -timmax -chname,{var},var -selname,{var} {ff} {target}"
 
             target = run_cdo(command, target = target)
 
-            new_files.append(ff)
-            new_commands.append(ff)
-
+            new_files.append(target)
+            new_commands.append(command)
 
         self.history.append(new_commands)
         self._hold_history = copy.deepcopy(self.history)
@@ -67,39 +63,52 @@ def phenology(self, var = None, metric = None, p = None):
         if len(self.current) == 1:
             self.current = self.current[0]
 
+        cleanup()
         return None
 
-    if metric == "peak":
-        target = temp_file(".nc")
-        command = f"cdo -L -timmin -setrtomiss,-10000,0 -expr,'peak=var*ctimestep()' -eq -chname,{var},var -selname,{var} {self.current} -timmax -chname,{var},var -selname,{var} {self.current} {target}"
 
-        target = run_cdo(command, target = target)
-        self.history.append(command)
-        self._hold_history = copy.deepcopy(self.history)
 
-        self.current = target
 
-        return None
+    if metric == "start" or metric == "end" or metric == "middle":
 
-    if metric == "start" or metric == "end":
+        if metric == "middle":
+            p = 50.0
+
+        if type(p) is int:
+            p = float(p)
+
         if type(p) is not float:
             raise TypeError("p is not float")
 
         start = (100 - p)/100
+        new_files = []
+        new_commands = []
 
-        target = temp_file(".nc")
-        command = f"cdo -L -timmin -setrtomiss,-10000,0 -expr,'middle=var*ctimestep()' -gt -timcumsum -chname,{var},var -selname,{var} {self.current} -multc,{start} -timsum -chname,{var},var -selname,{var} {self.current} {target}"
+        for ff in self:
 
-        target = run_cdo(command, target = target)
-        self.history.append(command)
+            if len(nc_years(ff)) > 1:
+                raise ValueError("This can only work with single year data currently")
+
+            target = temp_file(".nc")
+            command = f"cdo -L -timmin -setrtomiss,-10000,0 -expr,'{metric}=var*ctimestep()' -gt -timcumsum -chname,{var},var -selname,{var} {ff} -mulc,{start} -timsum -chname,{var},var -selname,{var} {ff} {target}"
+
+            target = run_cdo(command, target = target)
+
+            new_files.append(target)
+            new_commands.append(command)
+
+        self.history.append(new_commands)
         self._hold_history = copy.deepcopy(self.history)
-        self.current = target
+
+        self.current = new_files
+
+        if len(self.current) == 1:
+            self.current = self.current[0]
+        cleanup()
 
         return None
 
-    cleanup(self.current)
 
-    self.disk_clean()
-
+        raise ValueError("You have not supplied a valid metric")
 
 
