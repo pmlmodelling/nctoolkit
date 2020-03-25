f6e0b49a14ac63cdb424cede243a805ca19c9852
Robert Wilson <robertwilson190@gmail.com>
2020-03-25 10:25:37 +0000

    add ability to apply command to individual files or ensembles

--- nchack/nco_command.py
+++ nchack/nco_command.py
@@ -6,9 +6,10 @@ from .runthis import run_nco
 from .cleanup import cleanup
 from .cleanup import disk_clean
 from .session import nc_safe
+from .flatten import str_flatten
 
 
-def nco_command(self, command):
+def nco_command(self, command, ensemble = False):
     """
     Apply a cdo command
 
@@ -16,6 +17,8 @@ def nco_command(self, command):
     -------------
     command : string
         cdo command to call. This must be of the form cdo command infile outfile, where cdo, infile and outfile are attached later.
+    ensemble : boolean
+        Set to True if you want the command to take all of the files as input
     """
 
     # First, check that the command is valid
@@ -26,16 +29,30 @@ def nco_command(self, command):
     new_files = []
     new_commands = []
 
-    for ff in self:
+    if ensemble == False or len(self) == 1:
+        for ff in self:
 
-        target = temp_file(".nc")
+            target = temp_file(".nc")
 
-        the_command = f"{command} {ff} {target}"
+            the_command = f"{command} {ff} {target}"
 
-        target = run_nco(the_command, target = target)
+            target = run_nco(the_command, target = target)
 
-        new_files.append(target)
-        new_commands.append(the_command)
+            new_files.append(target)
+            new_commands.append(the_command)
+
+    else:
+            target = temp_file(".nc")
+
+            files = str_flatten(self.current, " ")
+
+            the_command = f"{command} {files} {target}"
+
+            target = run_nco(the_command, target = target)
+
+            new_files.append(target)
+            new_commands.append(the_command)
+
 
     self.current = new_files
 
