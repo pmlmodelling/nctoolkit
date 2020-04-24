e6ca24d87ac6006f031b427a91139d9bd0cc6cc8
Robert Wilson <robertwilson190@gmail.com>
2020-04-24 15:30:19 +0100

    remove redundant start_files

--- nchack/runthis.py
+++ nchack/runthis.py
@@ -243,8 +243,6 @@ def run_this(os_command, self, output = "one",  out_file = None):
 
     cores = session_info["cores"]
 
-    start_files = copy.deepcopy(self.current)
-
     if type(self.current) is str:
         output = "ensemble"
 
