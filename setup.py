b6a387974da2c408de5754c4063ea3a0d4e951eb
Robert Wilson <robertwilson190@gmail.com>
2020-05-04 12:08:09 +0100

    add cdo version check for install

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 from setuptools import setup
+import subprocess
+
+# check version of cdo installed
+
+
+cdo_check = subprocess.run("which cdo", shell = True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+cdo_check = str(cdo_check.stdout).replace("\\n", "")
+cdo_check = cdo_check.replace("b'", "").strip()
+if len(cdo_check) < 2:
+    raise ValueError("Please install cdo")
+
+cdo_check = subprocess.run("cdo --version", shell = True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+cdo_check = str(cdo_check.stderr).replace("\\n", "")
+cdo_check = cdo_check.replace("b'", "").strip()
+cdo_version = cdo_check.split("(")[0].strip().split(" ")[-1]
+if cdo_version not in ["1.9.8"]:
+    raise ValueError("Please install cdo version 1.9.8 or above")
 
 setup(name='nchack',
       version='0.1',
