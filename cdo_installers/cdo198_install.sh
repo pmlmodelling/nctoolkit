05fbd5414f57af182185977cb0d131ee36a8b193
Robert Wilson <robertwilson190@gmail.com>
2020-05-14 15:39:49 +0100

    add cdo installers scripts

---
+++ cdo_installers/cdo198_install.sh
@@ -0,0 +1,26 @@
+wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4/hdf5-1.8.13.tar.gz
+tar -xvf hdf5-1.8.13.tar.gz
+cd hdf5-1.8.13/
+./configure -with-zlib=/opt/cdo-install -prefix=/opt/cdo-install CFLAGS=-fPIC
+
+sudo make
+sudo make install> /dev/null
+cd ..
+
+wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.4.0.tar.gz
+tar -xvf netcdf-4.4.0.tar.gz
+cd netcdf-4.4.0/
+CPPFLAGS=-I/opt/cdo-install/include LDFLAGS=-L/opt/cdo-install/lib ./configure -prefix=/opt/cdo-install CFLAGS=-fPIC
+sudo make  > /dev/null
+sudo make install > /dev/null
+
+cd ..
+
+
+wget https://code.mpimet.mpg.de/attachments/download/20826/cdo-1.9.8.tar.gz
+tar -xvzf cdo-1.9.8.tar.gz
+cd cdo-1.9.8
+./configure -with-netcdf=/opt/cdo-install -with-hdf5=/opt/cdo-install
+sudo make
+sudo make install
+cd ..
