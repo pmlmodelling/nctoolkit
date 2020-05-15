1f28ac8fa86cdee014fad14d675d0d6255d4e5d8
Robert Wilson <robertwilson190@gmail.com>
2020-05-15 14:14:39 +0100

    remove sudo

--- cdo_installers/cdo195_install.sh
+++ cdo_installers/cdo195_install.sh
@@ -3,16 +3,16 @@ tar -xvf hdf5-1.8.13.tar.gz
 cd hdf5-1.8.13/
 ./configure -with-zlib=/opt/cdo-install -prefix=/opt/cdo-install CFLAGS=-fPIC
 
-sudo make
-sudo make install> /dev/null
+make
+make install
 cd ..
 
 wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.4.0.tar.gz
 tar -xvf netcdf-4.4.0.tar.gz
 cd netcdf-4.4.0/
 CPPFLAGS=-I/opt/cdo-install/include LDFLAGS=-L/opt/cdo-install/lib ./configure -prefix=/opt/cdo-install CFLAGS=-fPIC
-sudo make  > /dev/null
-sudo make install > /dev/null
+make
+make install
 
 cd ..
 
@@ -21,6 +21,6 @@ wget https://code.mpimet.mpg.de/attachments/download/18264/cdo-1.9.5.tar.gz
 tar -xvzf cdo-1.9.5.tar.gz
 cd cdo-1.9.5
 ./configure -with-netcdf=/opt/cdo-install -with-hdf5=/opt/cdo-install
-sudo make
-sudo make install
+make
+make
 cd ..
