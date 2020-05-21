1153c0179f9e9b654f4a11b7ea702fb1473a8ead
Robert Wilson <robertwilson190@gmail.com>
2020-05-21 14:39:06 +0100

    simplify

--- nctoolkit/delete.py
+++ nctoolkit/delete.py
@@ -9,13 +9,15 @@ from nctoolkit.show import nc_variables
 def remove_variables(self, vars=None):
     """
     Remove variables
+    This will remove stated variables from files in the dataset.
 
     Parameters
     -------------
     vars : str or list
-        Variable or variables to be removed from the data set. Variables that are listed but not in the dataset will be ignored
+        Variable or variables to be removed from the dataset. Variables that are listed but not in the dataset will be ignored
     """
 
+    # Some checks on the validity of variables supplied
     if vars is None:
         raise ValueError("Please supplied vars")
 
@@ -25,28 +27,6 @@ def remove_variables(self, vars=None):
     for vv in vars:
         if type(vv) is not str:
             raise TypeError(f"{vv} is not a str")
-
-    orig_vars = []
-    for ff in self:
-        orig_vars += nc_variables(ff)
-
-    missing_vars = [vv for vv in vars if vv not in orig_vars]
-
-    vars = [vv for vv in vars if vv in orig_vars]
-
-    if len(vars) == 0:
-        warnings.warn(message="None of the variables supplied are in the dataset")
-        return None
-    else:
-        if len(missing_vars) > 0:
-            if len(missing_vars) > 1:
-                warnings.warn(
-                    message=f"{str_flatten(missing_vars)} are not in the dataset!"
-                )
-            else:
-                warnings.warn(
-                    message=f"{str_flatten(missing_vars)} is not in the dataset!"
-                )
 
     vars = str_flatten(vars, ",")
 
