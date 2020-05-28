7b3b895859bdc58b95637e4ec922af55032e9a7c
Robert Wilson <robertwilson190@gmail.com>
2020-05-28 15:37:54 +0100

    make checks work when no cdo

--- nctoolkit/__init__.py
+++ nctoolkit/__init__.py
@@ -10,12 +10,6 @@ import subprocess
 
 # check version of cdo installed
 
-cdo_check = subprocess.run("which cdo", shell = True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-cdo_check = str(cdo_check.stdout).replace("\\n", "")
-cdo_check = cdo_check.replace("b'", "").strip()
-if len(cdo_check) < 2:
-    print("Please install CDO: https://code.mpimet.mpg.de/projects/cdo/")
-
 def valid(string):
         sub = "."
         wanted = ""
@@ -25,13 +19,18 @@ def valid(string):
         after = string[where:]
         after = after.replace(sub, wanted)
         newString = before + after
-        return float(newString) >= 1.99
+        return float(newString) >= 1.93
 
-
-cdo_check = subprocess.run("cdo --version", shell = True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-cdo_check = str(cdo_check.stderr).replace("\\n", "")
+cdo_check = subprocess.run("which cdo", shell = True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+cdo_check = str(cdo_check.stdout).replace("\\n", "")
 cdo_check = cdo_check.replace("b'", "").strip()
-cdo_version = cdo_check.split("(")[0].strip().split(" ")[-1]
-if valid(cdo_version) is False:
-    print("Please install CDO version 1.9.3 or above: https://code.mpimet.mpg.de/projects/cdo/")
+if len(cdo_check) < 2:
+    print("Please install CDO: https://code.mpimet.mpg.de/projects/cdo/")
+else:
+    cdo_check = subprocess.run("cdo --version", shell = True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    cdo_check = str(cdo_check.stderr).replace("\\n", "")
+    cdo_check = cdo_check.replace("b'", "").strip()
+    cdo_version = cdo_check.split("(")[0].strip().split(" ")[-1]
+    if valid(cdo_version) is False:
+        print("Please install CDO version 1.9.3 or above: https://code.mpimet.mpg.de/projects/cdo/")
 
