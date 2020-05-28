96b26b71f4f284119f9194bf1cc0f4f75238ac72
Robert Wilson <robertwilson190@gmail.com>
2020-05-28 11:28:07 +0100

    remove generate ensemble

--- nctoolkit/__init__.py
+++ nctoolkit/__init__.py
@@ -1,5 +1,5 @@
 from nctoolkit.api import open_data, merge, cor_time, cor_space, options, DataSet
 from nctoolkit.cleanup import cleanup, clean_all, deep_clean, temp_check
-from nctoolkit.create_ensemble import create_ensemble, generate_ensemble
+from nctoolkit.create_ensemble import create_ensemble
 from nctoolkit.session import show_session, session_files
 from nctoolkit.show import nc_variables, nc_years
--- tests/test_createns_lazy.py
+++ tests/test_createns_lazy.py
@@ -12,25 +12,9 @@ class TestCreate(unittest.TestCase):
         n = len(nc.session_files())
         self.assertEqual(n, 0)
 
-
-    def test_generate1(self):
-        x = nc.generate_ensemble("data/ensemble")
-
-        self.assertEqual(len(x), 1)
-
-
-    def test_recurse(self):
-        x = nc.generate_ensemble("data/ensemble1", recursive = True)
-        y = nc.generate_ensemble("data/ensemble1/data", recursive = False)
-
-        self.assertEqual(len(x), len(y))
-
-        with self.assertRaises(ValueError) as context:
-            z = nc.generate_ensemble("data/ensemble1/", recursive = False)
-
-    def test_error1(self):
-        with self.assertRaises(ValueError) as context:
-            x = nc.generate_ensemble("test1928")
+    def test_create(self):
+        ensemble = nc.create_ensemble("data/ensemble")
+        assert len(ensemble) == 60
 
 
 if __name__ == '__main__':
