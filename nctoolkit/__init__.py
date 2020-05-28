fda14f6ca313b0510a5e11b40f642a46825953e7
Robert Wilson <robertwilson190@gmail.com>
2020-05-28 14:59:47 +0100

    add check for whether acceptable cdo is installed

--- nctoolkit/__init__.py
+++ nctoolkit/__init__.py
@@ -1,5 +1,37 @@
+
 from nctoolkit.api import open_data, merge, cor_time, cor_space, options, DataSet
 from nctoolkit.cleanup import cleanup, clean_all, deep_clean, temp_check
 from nctoolkit.create_ensemble import create_ensemble
 from nctoolkit.session import show_session, session_files
 from nctoolkit.show import nc_variables, nc_years
+
+import re
+import subprocess
+
+# check version of cdo installed
+
+cdo_check = subprocess.run("which cdo", shell = True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+cdo_check = str(cdo_check.stdout).replace("\\n", "")
+cdo_check = cdo_check.replace("b'", "").strip()
+if len(cdo_check) < 2:
+    print("Please install CDO: https://code.mpimet.mpg.de/projects/cdo/")
+
+def valid(string):
+        sub = "."
+        wanted = ""
+        n = 3
+        where = [m.start() for m in re.finditer(sub, string)][n - 1]
+        before = string[:where]
+        after = string[where:]
+        after = after.replace(sub, wanted)
+        newString = before + after
+        return float(newString) >= 1.99
+
+
+cdo_check = subprocess.run("cdo --version", shell = True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+cdo_check = str(cdo_check.stderr).replace("\\n", "")
+cdo_check = cdo_check.replace("b'", "").strip()
+cdo_version = cdo_check.split("(")[0].strip().split(" ")[-1]
+if valid(cdo_version) is False:
+    print("Please install CDO version 1.9.3 or above: https://code.mpimet.mpg.de/projects/cdo/")
+
