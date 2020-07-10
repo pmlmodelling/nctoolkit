bdc0ff905fc72db404fae015dfe1af11dde1d5fe
Robert Wilson <robertwilson190@gmail.com>
2020-07-10 11:33:17 +0100

    further improvements

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
 from setuptools import Command, find_packages, setup
 
 
-DESCRIPTION = "Quick and efficient Python tools for analyzing NetCDF data"
+DESCRIPTION = "Efficient and intuitive tools for analyzing NetCDF data"
 LONG_DESCRIPTION = """
 **nctoolkit** is a Python package providing easy tools for manipulating NetCDF data.
 
-The goal of nctoolkit is to provide a comprehensive tool in Python for manipulating individual NetCDF files and ensembles of NetCDF files. The philosophy is to provide sufficient methods to carry out 80-90% of what you want to do with NetCDF files.
+nctoolkit is a comprehensive Python (3.6 and above) package for analyzing netCDF data.
 
-Under the hood, nctoolkit relies on the command line packages Climate Data Operates (CDO) and the NCO toolkit, but primarily on CDO. No prior knowledge of CDO or NCO are required to use nctoolkit. Behind the scenes, nctoolkit will generate system calls to either CDO or NCO, which are traced and can be viewed by the user. However, in almost all cases these can be ignored by most users.
+Core abilities of nctoolkit include:
+  - Clipping to spatial regions
+  - Calculating climatologies
+  - Subsetting to specific time periods
+  - Calculating spatial statistics
+  - Creating new variables using arithmetic operations
+  - Calculating anomalies
+  - Calculating rolling and cumulative statistics
+  - Horizontally and vertically remapping data
+  - Calculating time averages
+  - Interactive plotting of data
+  - Calculating the correlations between variables
+  - Calculating vertical statistics for the likes of oceanic data
+  - Calculating ensemble statistics
+  - Calculating phenological metrics
+
+Operation of the package requires the installation of Climate Data Operators (CDO). This is the computational backend for most of the methods used. No knowledge of CDO is required to use nctoolkit. A couple of methods provide users with the option of using netCDF Operators (NCO) as a backend. Guidance for how to install the backends are available `here <https://nctoolkit.readthedocs.io/en/stable/installing.html>`__.
 
 The package is designed for both intensive bulk processing of NetCDF files and interactive Jupyter notebook analysis. It features an interactive plotting feature which allows users to view the contents of NetCDF files either within Jupyter notebooks or a web browser.
 
@@ -25,7 +41,7 @@ REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 
 setup(name='nctoolkit',
-      version='0.1.2',
+      version='0.1.3',
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
       python_requires='>=3.6.1',
@@ -49,7 +65,6 @@ setup(name='nctoolkit',
         'setuptools-git',
         'wheel',
     ],
-      #packages=['nctoolkit'],
       install_requires = REQUIREMENTS,
       zip_safe=False)
 
