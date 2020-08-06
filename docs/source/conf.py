d7b8eb86a969480b0c94e0fb493e2f21be5114da
Robert Wilson <robertwilson190@gmail.com>
2020-08-06 12:59:17 +0100

    add tracker

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -91,7 +91,11 @@ pygments_style = 'sphinx'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+
+    'analytics_id': 'UA-46143561-3'
+
+        }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
