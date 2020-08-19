9752e31a61f795c3780ce81f40e7c45d6812a13e
Robert Wilson <robertwilson190@gmail.com>
2020-08-19 11:57:39 +0100

    add link to cdo conda install

--- nctoolkit/__init__.py
+++ nctoolkit/__init__.py
@@ -27,13 +27,16 @@ def valid(string):
 cdo_check = subprocess.run("which cdo", shell = True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 cdo_check = str(cdo_check.stdout).replace("\\n", "")
 cdo_check = cdo_check.replace("b'", "").strip()
+
 if len(cdo_check) < 2:
-    print("Please install CDO: https://code.mpimet.mpg.de/projects/cdo/")
+        print("Please install CDO version 1.9.3 or above: https://code.mpimet.mpg.de/projects/cdo/ or https://anaconda.org/conda-forge/cdo")
 else:
     cdo_check = subprocess.run("cdo --version", shell = True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     cdo_check = str(cdo_check.stderr).replace("\\n", "")
     cdo_check = cdo_check.replace("b'", "").strip()
     cdo_version = cdo_check.split("(")[0].strip().split(" ")[-1]
     if valid(cdo_version) is False:
-        print("Please install CDO version 1.9.3 or above: https://code.mpimet.mpg.de/projects/cdo/")
+        print("Please install CDO version 1.9.3 or above: https://code.mpimet.mpg.de/projects/cdo/ or https://anaconda.org/conda-forge/cdo")
+
+
 
