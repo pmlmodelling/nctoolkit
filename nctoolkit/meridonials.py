48a58610d325cff11171d158a9cc39d5000d7069
Robert Wilson <robertwilson190@gmail.com>
2020-09-25 10:26:17 +0100

    throw error for curvilinear files

--- nctoolkit/meridonials.py
+++ nctoolkit/meridonials.py
@@ -1,8 +1,28 @@
 from nctoolkit.runthis import run_this
+import subprocess
 
+def is_curvilinear(ff):
+    """Function to work out if a file contains a curvilinear grid"""
+    cdo_result = subprocess.run(
+        f"cdo sinfo {ff}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+
+    return (
+        len(
+            [
+                x
+                for x in cdo_result.stdout.decode("utf-8").split("\n")
+                if "curvilinear" in x
+            ]
+        )
+        > 0
+    )
 
 def zonstat(self, stat="mean"):
     """Method to calculate the meridonial statistic from a netcdf file"""
+    for ff in self:
+        if is_curvilinear(ff):
+            raise TypeError(f"meridonal_{stat} cannot be calculated for curvilinear grids.")
     cdo_command = f"cdo -mer{stat}"
 
     run_this(cdo_command, self, output="ensemble")
--- nctoolkit/zonals.py
+++ nctoolkit/zonals.py
@@ -1,8 +1,31 @@
 from nctoolkit.runthis import run_this
+import subprocess
+
+def is_curvilinear(ff):
+    """Function to work out if a file contains a curvilinear grid"""
+    cdo_result = subprocess.run(
+        f"cdo sinfo {ff}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+
+    return (
+        len(
+            [
+                x
+                for x in cdo_result.stdout.decode("utf-8").split("\n")
+                if "curvilinear" in x
+            ]
+        )
+        > 0
+    )
 
 
 def zonstat(self, stat="mean"):
     """Method to calculate the zonal statistic from a netcdf file"""
+
+    for ff in self:
+        if is_curvilinear(ff):
+            raise TypeError(f"zonal_{stat} cannot be calculated for curvilinear grids.")
+
     cdo_command = f"cdo -zon{stat}"
 
     run_this(cdo_command, self, output="ensemble")
