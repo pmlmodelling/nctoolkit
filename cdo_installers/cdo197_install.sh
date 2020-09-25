52ce748f64510325bedd6de549da0282032906f2
Robert Wilson <robertwilson190@gmail.com>
2020-09-25 11:49:27 +0100

    add 1.9.7 installer

---
+++ cdo_installers/cdo197_install.sh
@@ -0,0 +1,26 @@
+wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4/hdf5-1.8.13.tar.gz
+tar -xvf hdf5-1.8.13.tar.gz
+cd hdf5-1.8.13/
+./configure -with-zlib=/opt/cdo-install -prefix=/opt/cdo-install CFLAGS=-fPIC
+
+make
+make install
+cd ..
+
+wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.4.0.tar.gz
+tar -xvf netcdf-4.4.0.tar.gz
+cd netcdf-4.4.0/
+CPPFLAGS=-I/opt/cdo-install/include LDFLAGS=-L/opt/cdo-install/lib ./configure -prefix=/opt/cdo-install CFLAGS=-fPIC
+make
+make install
+
+cd ..
+
+
+wget https://code.mpimet.mpg.de/attachments/20124/cdo-1.9.7.1.tar.gz
+tar -xvzf cdo-1.9.8.tar.gz
+cd cdo-1.9.8
+./configure -with-netcdf=/opt/cdo-install -with-hdf5=/opt/cdo-install
+make
+make install
+cd ..
