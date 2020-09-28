5445ede8f5f5daeb01696fd1b29af02addc49b35
Robert Wilson <robertwilson190@gmail.com>
2020-09-28 16:27:31 +0100

    add centres method

---
+++ nctoolkit/centres.py
@@ -0,0 +1,57 @@
+from nctoolkit.runthis import run_this
+import subprocess
+
+def is_curvilinear(ff):
+    """Function to work out if a file contains a curvilinear grid"""
+    cdo_result = subprocess.run(
+        f"cdo sinfo {ff}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+
+    return (
+        len(
+            [
+                x
+                for x in cdo_result.stdout.decode("utf-8").split("\n")
+                if "curvilinear" in x
+            ]
+        )
+        > 0
+    )
+
+
+def centre(self, by ="latitude", by_area = False):
+    """
+    Calculate the zonal mean for each year/month combination in files.
+    This applies to each file in an ensemble.
+    """
+
+    if by not in ["longitude", "latitude"]:
+        raise ValueError("by is not valid. Please check!")
+    if type(by_area) is not bool:
+        raise ValueError("by_area is not boolean. Please check!")
+
+    self.run()
+
+    if type(self.current) is list:
+        raise TypeError("This method still does not work with lists! Consider merging.")
+
+    data1 = self.copy()
+    ops = dict()
+    for var in self.variables:
+        if by == "latitude":
+            ops[var] = (f"{var}*clat({var})")
+        else:
+            ops[var] = (f"{var}*clon({var})")
+
+    self.transmute(ops)
+    data1.spatial_sum(by_area = by_area)
+    self.spatial_sum(by_area = by_area)
+    self.divide(data1)
+
+
+
+
+
+
+
+
