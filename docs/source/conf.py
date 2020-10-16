3e96cefa02505097d89b47c28bc90c800740eef7
Robert Wilson <robertwilson190@gmail.com>
2020-10-16 16:55:16 +0100

    update versions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ author = 'Robert Wilson'
 
 # The short X.Y version
 # The full version, including alpha/beta/rc tags
-version = '0.2.0'
+version = '0.2.2'
 
 # -- General configuration ---------------------------------------------------
 
