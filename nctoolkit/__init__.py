3ebcf57a6da48ad63085c96cfa71d1f0c6f2d147
Robert Wilson <robertwilson190@gmail.com>
2020-10-19 09:38:56 +0100

    throw better error when cdo loading errors out

--- nctoolkit/__init__.py
+++ nctoolkit/__init__.py
@@ -35,23 +35,35 @@ def valid(string):
 
 
 cdo_check = subprocess.run(
-    "which cdo", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    "cdo --version", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
 )
-cdo_check = str(cdo_check.stdout).replace("\\n", "")
-cdo_check = cdo_check.replace("b'", "").strip()
 
-if len(cdo_check) < 2:
-    print(
-        "Please install CDO version 1.9.3 or above: https://code.mpimet.mpg.de/projects/cdo/ or https://anaconda.org/conda-forge/cdo"
-    )
-else:
-    cdo_check = subprocess.run(
-        "cdo --version", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-    cdo_check = str(cdo_check.stderr).replace("\\n", "")
+cdo_error = False
+if "error" in str(cdo_check.stderr):
+    error_message = str(cdo_check.stderr).replace("\\n", "").replace("b'", "").replace("cdo:", "").strip()
+    raise ValueError(f"Error loading CDO: {error_message}")
+
+if cdo_error is False:
+
+    cdo_check = str(cdo_check.stdout).replace("\\n", "")
     cdo_check = cdo_check.replace("b'", "").strip()
-    cdo_version = cdo_check.split("(")[0].strip().split(" ")[-1]
-    if valid(cdo_version) is False:
+
+
+    if "error" in cdo_check:
+         print(f"There is a problem with the CDO installation: {cdo_check}")
+
+    if len(cdo_check) < 2:
         print(
             "Please install CDO version 1.9.3 or above: https://code.mpimet.mpg.de/projects/cdo/ or https://anaconda.org/conda-forge/cdo"
         )
+    else:
+        cdo_check = subprocess.run(
+            "cdo --version", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        cdo_check = str(cdo_check.stderr).replace("\\n", "")
+        cdo_check = cdo_check.replace("b'", "").strip()
+        cdo_version = cdo_check.split("(")[0].strip().split(" ")[-1]
+        if valid(cdo_version) is False:
+            print(
+                "Please install CDO version 1.9.3 or above: https://code.mpimet.mpg.de/projects/cdo/ or https://anaconda.org/conda-forge/cdo"
+            )
