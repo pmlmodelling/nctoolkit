4d1446922afd9e89c64d475bab0e5d6354a1c970
Robert Wilson <robertwilson190@gmail.com>
2020-11-20 14:39:46 +0000

    remove redundant args

--- nctoolkit/plot.py
+++ nctoolkit/plot.py
@@ -2,7 +2,7 @@
 from ncplot import ncplot
 
 
-def plot(self, vars=None, log=False, panel=False):
+def plot(self, vars=None):
 
     """
     Autoplotting method.
@@ -10,24 +10,14 @@ def plot(self, vars=None, log=False, panel=False):
 
     Parameters
     -------------
-    log: boolean
-        Do you want a plotted data to be logged?
     vars: str or list
         A string or list of the variables to plot
-    panel: boolean
-        Do you want a panel plot, if avaiable?
     """
-
-    if type(log) is not bool:
-        raise TypeError("log is not boolean")
-
-    if type(panel) is not bool:
-        raise TypeError("panel is not boolean")
 
     self.run()
 
     if type(self.current) is list:
         raise TypeError("You cannot view multiple files!")
 
-    return ncplot(self.current)
+    return ncplot(self.current, vars = vars)
 
