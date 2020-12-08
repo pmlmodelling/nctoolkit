7d1e8c7be846e84fa291dd8f7944f7bc6a14b887
Robert Wilson <robertwilson190@gmail.com>
2020-12-08 14:35:04 +0000

    make zip work non-lazily

--- nctoolkit/zip.py
+++ nctoolkit/zip.py
@@ -1,4 +1,5 @@
 from nctoolkit.runthis import run_this
+from nctoolkit.session import session_info
 
 
 def zip(self):
@@ -7,3 +8,6 @@ def zip(self):
     This will compress the files within the dataset. This works lazily.
     """
     self._zip = True
+    if session_info["lazy"] == False:
+        self._execute = False
+        self.run()
