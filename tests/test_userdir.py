b4072266e8e46801f81a2dafcfe02cf1e7c59f21
Robert Wilson <robertwilson190@gmail.com>
2020-12-10 17:03:53 +0000

    add tests for user specified temp dirs

---
+++ tests/test_userdir.py
@@ -0,0 +1,32 @@
+import nctoolkit as nc
+
+nc.options(lazy=True)
+import pandas as pd
+import xarray as xr
+import os, pytest
+
+
+ff = "data/sst.mon.mean.nc"
+
+# This is a test to make sure we can change the temp dir to /var/tmp
+
+class TestSession:
+
+    def test_empty(self):
+        nc.options(temp_dir ="/var/tmp")
+        nc.options(lazy=False)
+        data = nc.open_data(ff)
+        data.spatial_mean()
+        data.mean()
+        nc.options(lazy=True)
+
+        data = nc.open_data(ff)
+        data.spatial_mean()
+        data.mean()
+        data.run()
+        assert data.current.startswith("/var/tmp")
+        nc.options(temp_dir = "/tmp")
+        del data
+        nc.cleanup()
+        n = len(nc.session_files())
+        assert n == 0
