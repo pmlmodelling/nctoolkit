70c61fab92b19fdb61fd0f9fcb28f195d9a7c8e6
Robert Wilson <robertwilson190@gmail.com>
2020-12-11 08:57:24 +0000

    only run test on linux

--- tests/test_userdir.py
+++ tests/test_userdir.py
@@ -4,6 +4,7 @@ nc.options(lazy=True)
 import pandas as pd
 import xarray as xr
 import os, pytest
+import session
 
 
 ff = "data/sst.mon.mean.nc"
@@ -12,21 +13,23 @@ ff = "data/sst.mon.mean.nc"
 
 class TestSession:
 
-    def test_empty(self):
-        nc.options(temp_dir ="/var/tmp")
-        nc.options(lazy=False)
-        data = nc.open_data(ff)
-        data.spatial_mean()
-        data.mean()
-        nc.options(lazy=True)
+    def test_userdir(self):
 
-        data = nc.open_data(ff)
-        data.spatial_mean()
-        data.mean()
-        data.run()
-        assert data.current.startswith("/var/tmp")
-        nc.options(temp_dir = "/tmp")
-        del data
-        nc.cleanup()
-        n = len(nc.session_files())
-        assert n == 0
+        if platform.system() == "Linux":
+            nc.options(temp_dir ="/var/tmp")
+            nc.options(lazy=False)
+            data = nc.open_data(ff)
+            data.spatial_mean()
+            data.mean()
+            nc.options(lazy=True)
+
+            data = nc.open_data(ff)
+            data.spatial_mean()
+            data.mean()
+            data.run()
+            assert data.current.startswith("/var/tmp")
+            nc.options(temp_dir = "/tmp")
+            del data
+            nc.cleanup()
+            n = len(nc.session_files())
+            assert n == 0
