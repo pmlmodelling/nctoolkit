00a3d2d0ee22a0f79283a130fa1567d7a3217258
Robert Wilson <robertwilson190@gmail.com>
2021-01-07 17:55:03 +0000

    add test for deep_clean

--- tests/test_temp.py
+++ tests/test_temp.py
@@ -1,8 +1,10 @@
 import nctoolkit as nc
 import subprocess
+import platform
 import pandas as pd
 import xarray as xr
 import os, pytest
+import random
 
 nc.options(lazy=True)
 
@@ -16,3 +18,21 @@ class TestTemp:
     def test_temp(self):
         with pytest.raises(TypeError):
             nc.temp_file.temp_file(1)
+
+        if platform.system() == "Linux":
+            print("here")
+            import random
+            rf = random.randint(1,10000000)
+            temp1 = f"/tmp/nctoolk{rf}chnctoolkittmpjs7aia2j.nc"
+            rf = random.randint(1,10000000)
+            temp2 = f"/tmp/nctoolk{rf}chnctoolkittmpjs7aia2j.nc"
+            os.mknod(temp1)
+            os.mknod(temp2)
+
+            assert os.path.exists(temp1)
+            assert os.path.exists(temp2)
+
+            nc.deep_clean()
+
+            assert os.path.exists(temp1) == False
+            assert os.path.exists(temp2) == False
