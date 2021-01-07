7102f2fa09d48a02a2d7a3db76a4d7386ae4e0d9
Robert Wilson <robertwilson190@gmail.com>
2021-01-07 17:13:58 +0000

    ensure deep_clean removes files from all sessions

--- nctoolkit/remove.py
+++ nctoolkit/remove.py
@@ -24,11 +24,11 @@ def nc_remove(ff, deep=False):
     if (ff in nc_safe) and (deep is False):
         raise ValueError(f"The file {ff} is in the safe list, so cannot be removed")
 
-    possibles = [os.path.basename(x) for x in temp_files]
-    if os.path.basename(ff) not in possibles:
-        raise ValueError(f"{ff} is not a temp file created by nctoolkit")
 
     if deep is False:
+        possibles = [os.path.basename(x) for x in temp_files]
+        if os.path.basename(ff) not in possibles:
+            raise ValueError(f"{ff} is not a temp file created by nctoolkit during this session")
         if session_info["stamp"] not in ff:
             raise ValueError(f"The file {ff}  was not created during this session")
 
