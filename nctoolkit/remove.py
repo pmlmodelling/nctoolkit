94fd016dd2b15b145dce2bb5608673163e4d0448
Robert Wilson <robertwilson190@gmail.com>
2021-01-12 10:19:55 +0000

    ensure nco temp files hanging round are deleted

--- nctoolkit/remove.py
+++ nctoolkit/remove.py
@@ -27,7 +27,7 @@ def nc_remove(ff, deep=False):
 
     if deep is False:
         possibles = [os.path.basename(x) for x in temp_files]
-        if os.path.basename(ff) not in possibles:
+        if (os.path.basename(ff).split(".")[0] + ".nc") not in possibles:
             raise ValueError(f"{ff} is not a temp file created by nctoolkit during this session")
         if session_info["stamp"] not in ff:
             raise ValueError(f"The file {ff}  was not created during this session")
