34d2a595d7fc6c4a75b765d3d1e26812490ce3aa
Robert Wilson <robertwilson190@gmail.com>
2021-01-13 11:32:31 +0000

    add tests for config file

--- tests/test_zzzz_lazy.py
+++ tests/test_zzzz_lazy.py
@@ -1,20 +1,49 @@
-import nctoolkit as nc
 import pandas as pd
 import xarray as xr
 import os, pytest
 
+import importlib
+import nctoolkit as nc
 
 ff = "data/sst.mon.mean.nc"
 
 
 class TestFinal:
     def test_cleanall(self):
-        safe = nc.session.nc_safe
-        tracker = nc.open_data(ff)
-        tracker.select_timesteps(0)
-        tracker.run()
-        safe = nc.session.nc_safe
-        nc.clean_all()
-        x = len([ff for ff in safe if os.path.exists(ff)])
+        from shutil import copyfile
+        copyfile("tests/.nctoolkitrc", ".nctoolkitrc")
+        importlib.reload(nc.api)
 
-        assert x == 0
+        print(nc.session.session_info)
+        os.remove(".nctoolkitrc")
+
+
+        assert nc.session.session_info["lazy"] == True
+        assert nc.session.session_info["cores"] == 6
+        assert nc.session.session_info["thread_safe"] == True
+        assert nc.session.session_info["user"] == "me"
+        assert nc.session.session_info["password"] == "pass"
+        assert nc.session.session_info["precision"] == "F32"
+
+        from os.path import expanduser
+        home = expanduser("~")
+
+        importlib.reload(nc.api)
+
+        assert nc.session.session_info["lazy"] == False
+
+        print(nc.session.session_info)
+
+        copyfile("tests/.nctoolkitrc", home+"/.nctoolkitrc")
+        importlib.reload(nc.api)
+
+        assert nc.session.session_info["lazy"] == True
+        assert nc.session.session_info["cores"] == 6
+        assert nc.session.session_info["thread_safe"] == True
+        assert nc.session.session_info["user"] == "me"
+        assert nc.session.session_info["password"] == "pass"
+        assert nc.session.session_info["precision"] == "F32"
+
+        os.remove( home+"/.nctoolkitrc")
+
+
