6012536410aa9eb95cb0c8dfabd2d672cc95922c
Robert Wilson <robertwilson190@gmail.com>
2021-01-19 11:53:25 +0000

    no error when file in nc_safe

--- nctoolkit/remove.py
+++ nctoolkit/remove.py
@@ -1,6 +1,7 @@
 import os
 
 from nctoolkit.session import session_info, nc_safe, temp_dirs, temp_files
+import warnings
 
 
 def nc_remove(ff, deep=False):
@@ -21,14 +22,12 @@ def nc_remove(ff, deep=False):
     if "nctoolkit" not in ff:
         raise ValueError(f"The file {ff}  was not created by nctoolkit")
 
+    # If things are running in parallel it's possible nc_safe was updated after files to delete
+    # were generated....
     if (ff in nc_safe) and (deep is False):
-        raise ValueError(f"The file {ff} is in the safe list, so cannot be removed")
-
+        return None
 
     if deep is False:
-        #possibles = [os.path.basename(x).split(".")[0] for x in temp_files]
-        #if (os.path.basename(ff).split(".")[0]) not in possibles:
-        #    raise ValueError(f"{ff} is not a temp file created by nctoolkit during this session")
         if session_info["stamp"] not in ff:
             raise ValueError(f"The file {ff}  was not created during this session")
 
