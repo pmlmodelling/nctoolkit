b9bf2993cd6d6472bc057453ab09b3fb4887385e
Robert Wilson <robertwilson190@gmail.com>
2021-01-20 13:10:51 +0000

    add examples for zip

--- nctoolkit/zip.py
+++ nctoolkit/zip.py
@@ -6,6 +6,15 @@ def zip(self):
     """
     Zip the dataset
     This will compress the files within the dataset. This works lazily.
+
+    Examples
+    ------------
+    If you want to zip the files in a dataset, do the following:
+
+        >>> data.zip()
+
+    This will occur lazily, so will only occur after everything has been evaluated.
+
     """
     self._zip = True
     if session_info["lazy"] == False:
