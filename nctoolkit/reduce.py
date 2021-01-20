3ba39c3b3dcddda55dcabb88c18ca896ccfc298e
Robert Wilson <robertwilson190@gmail.com>
2021-01-20 13:33:24 +0000

    add example for reduce

--- nctoolkit/reduce.py
+++ nctoolkit/reduce.py
@@ -6,6 +6,17 @@ def reduce_dims(self):
     Reduce dimensions of data
     This will remove any dimensions with only one value. For example, if only selecting
     one vertical level, the vertical dimension will be removed.
+
+    Examples
+    ------------
+    If you want to remove any dimensions that have only one value, do the following:
+
+        >>> data.reduce_dims("out.nc")
+
+    Note that this will work lazily. This method is most useful when you want to simplify
+    datasets before exporting them to something like a pandas dataframe.
+
+
     """
 
     if len(self.history) == len(self._hold_history):
