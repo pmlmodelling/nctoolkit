adadc048944ca5e552857485259eef282fea8003
Robert Wilson <robertwilson190@gmail.com>
2021-02-17 15:08:38 +0000

    add docstring for nc_remove, and some code comments

--- nctoolkit/remove.py
+++ nctoolkit/remove.py
@@ -1,14 +1,23 @@
 import os
-
-from nctoolkit.session import session_info, nc_safe, get_tempdirs, get_safe
 import warnings
-
+from nctoolkit.session import session_info, nc_safe, get_tempdirs, get_safe
 
 def nc_remove(ff, deep=False):
     """
     Method for removing netcdf files.
     This is ultra-safe and makes sure the file is in the tmp directory before deleting
+
+    Parameters
+    -------------
+    deep: bool
+        Set to True if you want nctoolkit to be able to delete files created in other sesions.
+        Default is False.
     """
+
+    # We need to make sure the file being deleted is from a temporary directory used
+    # in the current session. We need to throw an error if it's not
+    # Note: this code should be redundant. It is designed as a fail-safe to protect
+    # against bugs within nctoolkit causing files to be deleted when they shouldn't be
 
     unsafe = True
 
@@ -19,6 +28,8 @@ def nc_remove(ff, deep=False):
     if unsafe:
         raise ValueError(f"The file {ff} is not in a tmp folder")
 
+    # another fail-safe mechanism: ensure nctoolkit is in the file name
+
     if "nctoolkit" not in ff:
         raise ValueError(f"The file {ff}  was not created by nctoolkit")
 
@@ -27,15 +38,19 @@ def nc_remove(ff, deep=False):
     if (ff in get_safe()) and (deep is False):
         return None
 
-    if (deep is False):
+    if deep is False:
         for ss in list(get_safe()):
             if ss in ff:
                 return None
+
+    # another fail-safe mechanism: ensure the session-stamp is in the file name
 
     if deep is False:
         if session_info["stamp"] not in ff:
             raise ValueError(f"The file {ff}  was not created during this session")
 
+    # if things are going in parallel, it's possible the file will have been deleted by another process
+    # so use simple exception handling in case this has happend
     try:
         os.remove(ff)
     except:
