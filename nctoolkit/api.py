996c41a4eff41011d345e8fb11f1eec5303060f4
Robert  Wilson <robertwilson190@gmail.com>
2021-10-26 17:18:17 +0100

    remove superflous import

--- nctoolkit/api.py
+++ nctoolkit/api.py
@@ -12,7 +12,6 @@ import subprocess
 import warnings
 import urllib.request
 import platform
-import xarray as xr
 
 from netCDF4 import Dataset
 
@@ -382,27 +381,26 @@ def file_size(file_path):
         file_info = os.stat(file_path)
         return file_info.st_size
 
+
 def from_xarray(ds):
 
     """
-    Convert an xarray dataset to an nctoolkit dataset 
+    Convert an xarray dataset to an nctoolkit dataset
+    This will first save the xarray dataset as a temporary netCDF file.
 
     Parameters
     ---------------
-    ds : xarray dataset 
+    ds : xarray dataset
     ---------------
     from_xarray : nctoolkit.DataSet
     """
 
     ff = temp_file(".nc")
     ds.to_netcdf(ff)
-    
+
     d = DataSet(ff)
     return d
-    
 
-
-    
 
 def open_data(x=[], checks=False, **kwargs):
     """
