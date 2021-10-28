8292a7f78917805b2cbd5f186af17bde123eba25
Robert  Wilson <robertwilson190@gmail.com>
2021-10-28 10:26:07 +0100

    update to v0.3.9

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 
 setup(name='nctoolkit',
-      version='0.3.8',
+      version='0.3.9',
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
       python_requires='>=3.6.1',
