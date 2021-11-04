d955adc557628aadbb84c2416b13f2ae49142eb2
Robert  Wilson <robertwilson190@gmail.com>
2021-11-04 12:09:43 +0000

    ensure tests don't wipe other nctoolkit sessions

--- tests/test_temp.py
+++ tests/test_temp.py
@@ -2,6 +2,7 @@ import nctoolkit as nc
 import subprocess
 import platform
 import pandas as pd
+import glob
 import xarray as xr
 import os, pytest
 import random
@@ -20,19 +21,29 @@ class TestTemp:
             nc.temp_file.temp_file(1)
 
         if platform.system() == "Linux":
-            print("here")
-            import random
-            rf = random.randint(1,10000000)
-            temp1 = f"/tmp/nctoolk{rf}chnctoolkittmpjs7aia2j.nc"
-            rf = random.randint(1,10000000)
-            temp2 = f"/tmp/nctoolk{rf}chnctoolkittmpjs7aia2j.nc"
-            os.mknod(temp1)
-            os.mknod(temp2)
+            candidates = []
+            for directory in nc.session.get_tempdirs():
+                mylist = [f for f in glob.glob(f"{directory}/*")]
+                for ff in mylist:
+                    candidates.append(ff)
+        
+            mylist = [f for f in candidates if "nctoolkit" in f and nc.session.session_info["stamp"] not in f]
 
-            assert os.path.exists(temp1)
-            assert os.path.exists(temp2)
+            if len(mylist) == 0:
+            
 
-            nc.deep_clean()
+                import random
+                rf = random.randint(1,10000000)
+                temp1 = f"/tmp/nctoolk{rf}chnctoolkittmpjs7aia2j.nc"
+                rf = random.randint(1,10000000)
+                temp2 = f"/tmp/nctoolk{rf}chnctoolkittmpjs7aia2j.nc"
+                os.mknod(temp1)
+                os.mknod(temp2)
 
-            assert os.path.exists(temp1) == False
-            assert os.path.exists(temp2) == False
+                assert os.path.exists(temp1)
+                assert os.path.exists(temp2)
+        
+                nc.deep_clean()
+
+                assert os.path.exists(temp1) == False
+                assert os.path.exists(temp2) == False
