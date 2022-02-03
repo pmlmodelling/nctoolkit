7fffb400c6ce6e9ad74b04bf0636d0bcc65cfedb
Robert  Wilson <robertwilson190@gmail.com>
2022-02-03 17:27:06 +0000

    switch to 0.4.1

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 
 setup(name='nctoolkit',
-      version='0.4.0',
+      version='0.4.1',
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
       python_requires='>=3.6.1',
