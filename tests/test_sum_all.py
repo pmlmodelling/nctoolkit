5a5b1270c7dc720c482400ccfa1573bb5c54d1e0
Robert Wilson <robertwilson190@gmail.com>
2022-03-03 17:29:17 +0000

    add tests for sumall

---
+++ tests/test_sum_all.py
@@ -0,0 +1,20 @@
+import nctoolkit as nc
+import numpy as np
+import pandas as pd
+import xarray as xr
+import os, pytest
+
+nc.options(lazy=True)
+
+
+ff = "data/sst.mon.mean.nc"
+
+
+class TestClip:
+    def test_centre(self):
+        ds = nc.open_data(["data/2003.nc", "data/2004.nc"])
+        ds.select(time = 0)
+        ds.assign(tos1 = lambda x: -x.analysed_sst)
+        ds.sum_all()
+        ds.merge("time")
+        assert ds.to_dataframe().total.abs().max() < 1e-4
