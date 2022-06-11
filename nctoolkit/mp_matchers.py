9d9ebfc05dc23efb5d26e5cd3f633a2d6abd3fd8
Robert  Wilson <robertwilson190@gmail.com>
2022-06-11 12:41:38 +0100

    simply doc text

--- nctoolkit/mp_matchers.py
+++ nctoolkit/mp_matchers.py
@@ -9,21 +9,21 @@ def match_points(self, df = None, variables = None, depths = None, tmean = False
     Match dataset to a spatiotemporal points dataframe
     Parameters
     -------------
-    df: pandas dataframe containing the spatiotemporal points to match with.
+    df: pandas DataFrame
         The column names must be made up of a subset of "lon", "lat", "year", "month", "day" and "depth"
-    variables: str or list
+    variables:  str or list
         Str or list of variables. All variables are matched up if this is not supplied.
-    depths:  nctoolkit dataset or list giving depths
+    depths:  nctoolkit DataSet or list
         If each cell has different vertical levels, this must be provided as a dataset.
         If each cell has the same vertical levels, provide it as a list.
         If this is not supplied nctoolkit will try to figure out what they are.
         Only required if carrying out vertical matchups.
-    tmean: bool
+    tmean:  bool
         Set to True or False, depending on whether you want temporal averaging at the temporal resolution given by df.
         For example, if you only had months in df, but had daily data in ds, you might want to calculate a daily average in the
         monthly dataset.
         This is equivalent to apply `ds.tmean(..)` to the dataset.
-    top: bool
+    top:    bool
         Set to True if you want only the top/surface level of the dataset to be selected for matching.
     nan: float or list
         Value or range of values to set to nan. Defaults to 0.
