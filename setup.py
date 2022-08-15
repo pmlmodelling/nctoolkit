d608c42d1c27c3d88ac3e8a8bab2d9658b512a9c
Robert  Wilson <robertwilson190@gmail.com>
2022-08-15 08:52:07 +0100

    switch to v 0.6.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 
 setup(name='nctoolkit',
-      version='0.5.4',
+      version='0.6.0',
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
       python_requires='>=3.6.1',
