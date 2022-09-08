42e57e10ae86c6e02f2a0b41dfad615edaf65e2c
Robert  Wilson <robertwilson190@gmail.com>
2022-09-08 14:57:31 +0100

    add plot test

---
+++ tests/test_plot.py
@@ -0,0 +1,26 @@
+import nctoolkit as nc
+import numpy as np
+import pandas as pd
+import xarray as xr
+import os, pytest
+import re
+
+nc.options(lazy=True)
+
+
+ff = "data/sst.mon.mean.nc"
+
+class TestCrop:
+    def test_plot(self):
+        ds = nc.open_data(ff)
+        ds.subset(time = 0)
+        out_file = nc.temp_file.temp_file(".html")
+        ds.plot(out= out_file)
+        file1 = open('data/test1.html', 'r')
+        lines_1 = file1.readlines()
+        file1 = open(out_file, 'r')
+        lines_2 = file1.readlines()
+        text = re.compile('"#.[0-9, a-z]*"')
+        for i in range(len(lines_1)):
+            assert text.findall(lines_1[i]) == text.findall(lines_2[i]) 
+        os.remove(out_file)
