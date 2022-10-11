8438f2fec9e55034eb6373ddff8b5936648e2b6a
Robert  Wilson <robertwilson190@gmail.com>
2022-10-11 16:14:40 +0100

    improve handling of levels in to_zlevels

--- nctoolkit/tozlev.py
+++ nctoolkit/tozlev.py
@@ -11,7 +11,7 @@ from nctoolkit.temp_file import temp_file
 from nctoolkit.session import append_safe
 from nctoolkit.session import remove_safe
 
-def to_zlevels(self, levels = None, thickness = None, depths = None):
+def to_zlevels(self, levels = None, thickness = None):
     """
     Convert datasets with non z-level verticals to z-levels 
     Experimental method: Use at your own risk.
@@ -25,26 +25,21 @@ def to_zlevels(self, levels = None, thickness = None, depths = None):
         One of: a variable, in the dataset, which contains the variable thicknesses; a .nc file which contains
         the thicknesses; or a Dataset that contains the thicknesses. Note: the .nc file or Dataset must only contain
         one variable. Thickness should be in metres. Vertical interpolation will take the value from the mid-point of the level.
-    depths: str or Dataset
-        One of: a variable, in the dataset, which contains the depths of each cell; a .nc file which contains
-        the depths; or a Dataset that contains the depths. Note: the .nc file or Dataset must only contain
-        one variable.
     """
     self.run()
 
     if len(self) > 1:
         raise ValueError("This currently only works on single file datasets")
 
-    if thickness is None and depths is None:
+    if thickness is None:
         if "e3t" in self.variables:
             thickness = "e3t"
 
     drop_this = None
 
-    if depths is None:
-        if "api.DataSet" not in str(type(thickness)):
-            if thickness is None or type(thickness) is not str:
-                raise ValueError("Please provide a valid thickness or depths variable")
+    if "api.DataSet" not in str(type(thickness)):
+        if thickness is None or type(thickness) is not str:
+            raise ValueError("Please provide a valid thickness or depths variable")
 
     if thickness is None:
         if "api.DataSet" not in str(type(depths)):
@@ -62,62 +57,48 @@ def to_zlevels(self, levels = None, thickness = None, depths = None):
 
     sorted = False
 
-    if depths is None:
-
-        if "api.DataSet" in str(type(thickness)):
-            ds_depths = thickness.copy()
-            ds_depths.run()
-            if len(ds_depths.variables) != 1:
-                raise ValueError("Please provide a thickness dataset with 1 variable!")
-            sorted = True
-
-        if sorted is False:
-            if thickness in self.variables:
-                ds_depths = open_data(self1[0])
-                ds_depths.subset(variable=thickness)
-                ds_depths.run()
-                drop_this = thickness
-            else:
-                ds_depths = open_data(thickness)
-                if len(ds_depths.variables) != 1:
-                    raise ValueError("Please provide a thickness file with 1 variable!")
-
-        thick_var = ds_depths.variables[0]
-
-        ds_depths.rename({thick_var: "thickness"})
+    if "api.DataSet" in str(type(thickness)):
+        ds_depths = thickness.copy()
         ds_depths.run()
-        ds_depths.vertical_cumsum()
-        ds_depths.rename({"thickness": "depth"})
-        ds_depths.subset(times = 0)
-        ds_depths.cdo_command("setmisstoc,-9999999")
-        ds_depths.run()
+        if len(ds_depths.variables) != 1:
+            raise ValueError("Please provide a thickness dataset with 1 variable!")
+        sorted = True
 
-    if depths is not None:
-        if "api.DataSet" in str(type(depths)):
-            ds_depths = depths.copy()
-            ds_depths.cdo_command("setmisstoc,-9999999")
+    if sorted is False:
+        if thickness in self.variables:
+            ds_depths = open_data(self1[0])
+            ds_depths.subset(variable=thickness)
             ds_depths.run()
+            drop_this = thickness
+        else:
+            ds_depths = open_data(thickness)
             if len(ds_depths.variables) != 1:
-                raise ValueError("Please provide a depths dataset with 1 variable!")
-            sorted = True
+                raise ValueError("Please provide a thickness file with 1 variable!")
 
-        if sorted is False:
-            if thickness in self.variables:
-                ds_depths = open_data(self1[0])
-                ds_depths.subset(variable=thickness)
-                ds_depths.cdo_command("setmisstoc,-9999999")
-                ds_depths.run()
-                drop_this = thickness
-            else:
-                ds_depths = open_data(depths)
-                ds_depths.cdo_command("setmisstoc,-9999999")
-                ds_depths.run()
-                if len(ds_depths.variables) != 1:
-                    raise ValueError("Please provide a thickness file with 1 variable!")
+    thick_var = ds_depths.variables[0]
+
+    ds_depths.rename({thick_var: "thickness"})
+    ds_depths.run()
+    ds_thick = ds_depths.copy()
+    ds_thick.divide(2)
+    ds_depths.vertical_cumsum()
+    ds_depths.rename({"thickness": "depth"})
+    ds_depths.subtract(ds_thick)
+    ds_depths.assign(depth = lambda x: x.depth * (vertical_min(x.depth) < x.depth) * (isnan(x.depth) == False))
+    ds_depths.subset(times = 0)
+    ds_depths.cdo_command("setmisstoc,-9999999")
+    ds_depths.run()
 
 
     zaxis = temp_file().replace(".", "")
     append_safe(zaxis)
+
+    if type(levels) is not list:
+        raise TypeError("levels must be a list")
+
+    for ll in levels:
+        if ll < 0:
+            raise ValueError("levels must not have negative values")
 
 
     line3 = "levels = " + " ".join([str(x) for x in levels]) + " \n"
@@ -130,13 +111,18 @@ def to_zlevels(self, levels = None, thickness = None, depths = None):
     target = ds_depths.copy()
     target.assign(depth = lambda x: level(x.depth) + 0 * (x.depth == x.depth), drop = True)
     target.rename({"depth_2":"depth"})
+    target.run()
+
+
     target.vertical_interp(levels = levels)
+    target.assign(depth = lambda x: level(x.depth) + 0 * (x.depth == x.depth), drop = True)
     target.run()
 
     out = temp_file(".nc")
     append_safe(out)
 
     command = f"cdo intlevel3d,{target[0]} {ds[0]}  {ds_depths[0]} {out}"
+
 
     run_cdo(command, target = out, precision = self._precision)
 
