fb1af000d617f5d2e02702ce6b385069d79f0022
Robert  Wilson <robertwilson190@gmail.com>
2023-06-09 10:33:33 +0100

    try alabaster rdt theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ extensions = [
 
 #html_theme = "sphinx_rtd_theme"
 #html_theme = "furo"
-html_theme = "piccolo_theme"
+html_theme = "alabaster"
 html_static_path = ['_static']
 html_logo = "_static/nctoolkit_logo.jpg"
 
