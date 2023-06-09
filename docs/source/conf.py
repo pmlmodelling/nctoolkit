46ee68dca89c39d2721388da47711dd63bcf5dec
Robert  Wilson <robertwilson190@gmail.com>
2023-06-09 11:09:30 +0100

    try material

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ extensions = [
 
 #html_theme = "sphinx_rtd_theme"
 #html_theme = "furo"
-html_theme = "insipid"
+html_theme = "sphinx_material"
 html_static_path = ['_static']
 html_logo = "_static/nctoolkit_logo.jpg"
 
