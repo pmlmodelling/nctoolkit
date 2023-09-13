741edd33848f49b272dea938804b49c20ac4bc59
Robert  Wilson <robertwilson190@gmail.com>
2023-09-13 12:47:33 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ extras_require["complete"] = ["geoviews", "rioxarray", "cfchecker", "geocube", "
 REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 setup(name='nctoolkit',
-      version='1.1.0',
+      version='1.1.1',
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
       python_requires='>=3.6.1',
