4816404bfedc5cfd5a59939fd59026c5b3bd89db
Robert  Wilson <robertwilson190@gmail.com>
2023-11-02 10:45:21 +0000

    revert to original anom test after fixing python version in osx tests

--- tests/test_anomaly_lazy.py
+++ tests/test_anomaly_lazy.py
@@ -74,94 +74,94 @@ class TestAnomaly:
         assert n == 0
         assert nc.session.session_info["parallel"] == False
 
-    # def test_error4(self):
-    #     ff = "data/sst.mon.mean.nc"
-    #     tracker = nc.open_data(ff, checks = False)
-    #     with pytest.raises(ValueError):
-    #         tracker.monthly_anomaly(baseline=[1, 2, 3])
-    #     n = len(nc.session_files())
-    #     assert n == 0
+    def test_error4(self):
+        ff = "data/sst.mon.mean.nc"
+        tracker = nc.open_data(ff, checks = False)
+        with pytest.raises(ValueError):
+            tracker.monthly_anomaly(baseline=[1, 2, 3])
+        n = len(nc.session_files())
+        assert n == 0
 
-    # def test_error5(self):
-    #     ff = "data/sst.mon.mean.nc"
-    #     tracker = nc.open_data(ff, checks = False)
-    #     with pytest.raises(TypeError):
-    #         tracker.annual_anomaly(baseline=[1, "x"])
-    #     n = len(nc.session_files())
-    #     assert n == 0
+    def test_error5(self):
+        ff = "data/sst.mon.mean.nc"
+        tracker = nc.open_data(ff, checks = False)
+        with pytest.raises(TypeError):
+            tracker.annual_anomaly(baseline=[1, "x"])
+        n = len(nc.session_files())
+        assert n == 0
 
-    # def test_error6(self):
-    #     ff = "data/sst.mon.mean.nc"
-    #     tracker = nc.open_data(ff, checks = False)
-    #     with pytest.raises(TypeError):
-    #         tracker.annual_anomaly(baseline=["x", "x"])
-    #     n = len(nc.session_files())
-    #     assert n == 0
+    def test_error6(self):
+        ff = "data/sst.mon.mean.nc"
+        tracker = nc.open_data(ff, checks = False)
+        with pytest.raises(TypeError):
+            tracker.annual_anomaly(baseline=["x", "x"])
+        n = len(nc.session_files())
+        assert n == 0
 
-    # def test_error7(self):
-    #     ff = "data/sst.mon.mean.nc"
-    #     tracker = nc.open_data(ff, checks = False)
-    #     with pytest.raises(ValueError):
-    #         tracker.annual_anomaly(baseline=[1990, 1980])
-    #     n = len(nc.session_files())
-    #     assert n == 0
+    def test_error7(self):
+        ff = "data/sst.mon.mean.nc"
+        tracker = nc.open_data(ff, checks = False)
+        with pytest.raises(ValueError):
+            tracker.annual_anomaly(baseline=[1990, 1980])
+        n = len(nc.session_files())
+        assert n == 0
 
-    # def test_error8(self):
-    #     ff = "data/sst.mon.mean.nc"
-    #     tracker = nc.open_data(ff, checks = False)
-    #     with pytest.raises(ValueError):
-    #         tracker.annual_anomaly(baseline=[1000, 1990])
-    #     n = len(nc.session_files())
-    #     n == 0
+    def test_error8(self):
+        ff = "data/sst.mon.mean.nc"
+        tracker = nc.open_data(ff, checks = False)
+        with pytest.raises(ValueError):
+            tracker.annual_anomaly(baseline=[1000, 1990])
+        n = len(nc.session_files())
+        n == 0
 
-    # def test_error9(self):
-    #     ff = "data/sst.mon.mean.nc"
-    #     tracker = nc.open_data(ff, checks = False)
-    #     with pytest.raises(ValueError):
-    #         tracker.annual_anomaly(baseline=[1980, 1990], metric="x")
-    #     n = len(nc.session_files())
-    #     assert n == 0
+    def test_error9(self):
+        ff = "data/sst.mon.mean.nc"
+        tracker = nc.open_data(ff, checks = False)
+        with pytest.raises(ValueError):
+            tracker.annual_anomaly(baseline=[1980, 1990], metric="x")
+        n = len(nc.session_files())
+        assert n == 0
 
-    # def test_error10(self):
-    #     ff = "data/sst.mon.mean.nc"
-    #     tracker = nc.open_data(ff, checks = False)
-    #     with pytest.raises(TypeError):
-    #         tracker.monthly_anomaly(baseline=[1, "x"])
-    #     n = len(nc.session_files())
-    #     assert n == 0
+    def test_error10(self):
+        ff = "data/sst.mon.mean.nc"
+        tracker = nc.open_data(ff, checks = False)
+        with pytest.raises(TypeError):
+            tracker.monthly_anomaly(baseline=[1, "x"])
+        n = len(nc.session_files())
+        assert n == 0
 
-    # def test_error11(self):
-    #     ff = "data/sst.mon.mean.nc"
-    #     tracker = nc.open_data(ff, checks = False)
-    #     with pytest.raises(TypeError):
-    #         tracker.monthly_anomaly(baseline=["x", "x"])
-    #     n = len(nc.session_files())
-    #     assert n == 0
+    def test_error11(self):
+        ff = "data/sst.mon.mean.nc"
+        tracker = nc.open_data(ff, checks = False)
+        with pytest.raises(TypeError):
+            tracker.monthly_anomaly(baseline=["x", "x"])
+        n = len(nc.session_files())
+        assert n == 0
 
-    # def test_error12(self):
-    #     ff = "data/sst.mon.mean.nc"
-    #     tracker = nc.open_data(ff, checks = False)
-    #     with pytest.raises(ValueError):
-    #         tracker.monthly_anomaly(baseline=[1990, 1980])
-    #     n = len(nc.session_files())
-    #     assert n == 0
+    def test_error12(self):
+        ff = "data/sst.mon.mean.nc"
+        tracker = nc.open_data(ff, checks = False)
+        with pytest.raises(ValueError):
+            tracker.monthly_anomaly(baseline=[1990, 1980])
+        n = len(nc.session_files())
+        assert n == 0
 
-    # def test_error13(self):
-    #     ff = "data/sst.mon.mean.nc"
-    #     tracker = nc.open_data(ff, checks = False)
-    #     with pytest.raises(ValueError):
-    #         tracker.monthly_anomaly(baseline=[1000, 1990])
-    #     n = len(nc.session_files())
-    #     assert n == 0
+    def test_error13(self):
+        ff = "data/sst.mon.mean.nc"
+        tracker = nc.open_data(ff, checks = False)
+        with pytest.raises(ValueError):
+            tracker.monthly_anomaly(baseline=[1000, 1990])
+        n = len(nc.session_files())
+        assert n == 0
 
-    # def test_empty(self):
-    #     n = len(nc.session_files())
-    #     assert n == 0
+    def test_empty(self):
+        n = len(nc.session_files())
+        assert n == 0
 
-    # def test_error_window(self):
-    #     ff = "data/sst.mon.mean.nc"
-    #     tracker = nc.open_data(ff, checks = False)
-    #     with pytest.raises(TypeError):
-    #         tracker.annual_anomaly(baseline=[1970, 1979], window="x")
-    #     with pytest.raises(TypeError):
-    #         tracker.annual_anomaly(baseline=[1970, 1979], window=0)
+    def test_error_window(self):
+        ff = "data/sst.mon.mean.nc"
+        tracker = nc.open_data(ff, checks = False)
+        with pytest.raises(TypeError):
+            tracker.annual_anomaly(baseline=[1970, 1979], window="x")
+        with pytest.raises(TypeError):
+            tracker.annual_anomaly(baseline=[1970, 1979], window=0)
