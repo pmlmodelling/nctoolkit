f86c5fb72f1e95e652031350b47baf95336966ce
Robert  Wilson <robertwilson190@gmail.com>
2023-11-02 10:10:10 +0000

    add test for parallel setting in anom

--- tests/test_anomaly_lazy.py
+++ tests/test_anomaly_lazy.py
@@ -8,6 +8,7 @@ import os, pytest
 
 class TestAnomaly:
     def test_relative(self):
+        assert nc.session.session_info["parallel"] == False
         n = len(nc.session_files())
         assert n == 0
         ff = "data/sst.mon.mean.nc"
@@ -23,8 +24,10 @@ class TestAnomaly:
         n = len(nc.session_files())
         del tracker
         assert n == 1
+        assert nc.session.session_info["parallel"] == False
 
     def test_error1(self):
+        assert nc.session.session_info["parallel"] == False
         n = len(nc.session_files())
         assert n == 0
         ff = "data/sst.mon.mean.nc"
@@ -44,22 +47,27 @@ class TestAnomaly:
             tracker.monthly_anomaly(baseline=[1970, 1979])
         n = len(nc.session_files())
         assert n == 0
+        assert nc.session.session_info["parallel"] == False
 
     def test_error2(self):
+        assert nc.session.session_info["parallel"] == False
         ff = "data/sst.mon.mean.nc"
         tracker = nc.open_data(ff, checks = False)
         with pytest.raises(TypeError):
             tracker.monthly_anomaly(baseline="x")
         n = len(nc.session_files())
         assert n == 0
+        assert = nc.session.session_info["parallel"] == False
 
     def test_error3(self):
         ff = "data/sst.mon.mean.nc"
+        assert nc.session.session_info["parallel"] == False
         tracker = nc.open_data(ff, checks = False)
         with pytest.raises(ValueError):
             tracker.annual_anomaly(baseline=[1, 2, 3])
         n = len(nc.session_files())
         assert n == 0
+        assert nc.session.session_info["parallel"] == False
 
     # def test_error4(self):
     #     ff = "data/sst.mon.mean.nc"
