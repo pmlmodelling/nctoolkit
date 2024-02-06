cb807af46f4c7d1cd0839597f02b8fb63aa918cf
Robert  Wilson <robertwilson190@gmail.com>
2024-02-06 11:29:07 +0000

    add test to make sure cheat sheet version is up to date

---
+++ tests/test_cheat.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import xarray as xr
+import os, pytest
+import textract
+
+import importlib
+import nctoolkit as nc
+
+
+
+class TestFinal:
+    def test_cheat(self):
+
+        # only run this test on linux
+        if os.name == "posix":
+            assert len(nc.session_files()) == 0
+            assert len(nc.session.get_safe()) == 0
+            ff = "../cheatsheet/nctoolkit_cheatsheet.pdf"
+            assert len(nc.session_files()) == 0
+            assert len(nc.session.get_safe()) == 0
+            ff = "cheatsheet/nctoolkit_cheatsheet.pdf"
+# read p    df ff
+            text = textract.process(ff, method='pdfminer')
+            text = text.decode('utf-8')
+            # version format is v*.*.*. Find it in text
+            import re
+            pattern = re.compile(r'v\d+\.\d+\.\d+')
+            version = pattern.search(text)
+            version = version.group()
+            version = version.replace("v", "")
+            the_version = nc.__version__
+            assert version == the_version
+
+
