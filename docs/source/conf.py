fb061b2de0ba766a39942b9648127a27a305b553
Robert  Wilson <robertwilson190@gmail.com>
2024-04-04 09:59:33 +0100

    switch to default rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,9 +39,9 @@ extensions = [
     'sphinx.ext.githubpages',
 ]
 
-#html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 #html_theme = "furo"
-html_theme = 'sphinx_book_theme'
+#html_theme = 'sphinx_book_theme'
 
 html_static_path = ['_static']
 html_logo = "_static/nctoolkit_logo.jpg"
